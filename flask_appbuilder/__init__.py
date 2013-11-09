59363464ed37c84dc0a281868ee38f151c3de279
dpgaspar <danielvazgaspar@gmail.com>
2013-11-09 23:56:35 +0000

    Pypi Support (initial)

--- flask_appbuilder/__init__.py
+++ flask_appbuilder/__init__.py
@@ -1,3 +1,3 @@
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __author__ = 'Daniel Vaz Gaspar'
 __email__ = 'danielvazgaspar@gmail.com'
--- setup.py
+++ setup.py
@@ -5,44 +5,21 @@ import os
 from setuptools import setup, find_packages
 
 
-def fpath(name):
-    return os.path.join(os.path.dirname(__file__), name)
-
-
-def read(fname):
-    return open(fpath(fname)).read()
-
-
-def desc():
-    info = read('README.md')
-    try:
-        return info + '\n\n' + read('doc/changelog.rst')
-    except IOError:
-        return info
-
-# grep flask_admin/__init__.py since python 3.x cannot import it before using 2to3
-file_text = read(fpath('flask_appbuilder/__init__.py'))
-def grep(attrname):
-    pattern = r"{0}\W*=\W*'([^']+)'".format(attrname)
-    strval, = re.findall(pattern, file_text)
-    return strval
-
-
 setup(
     name='Flask-AppBuilder',
-    version=grep('__version__'),
+    version='0.0.2',
     url='https://github.com/dpgaspar/flask-appbuilder/',
     license='BSD',
-    author=grep('__author__'),
-    author_email=grep('__email__'),
+    author='Daniel Vaz Gaspar',
+    author_email='danielvazgaspar@gmail.com',
     description='Simple and rapid Application builder, includer detailed security, auto form generation, google charts and much more.',
-    long_description=desc(),
-    packages=find_packages(),
+    long_description=__doc__,
+    packages=['flask_appbuilder'],
     include_package_data=True,
     zip_safe=False,
     platforms='any',
     install_requires=[
-        'Flask>=0.7',
+        'Flask>=0.10',
         'Flask-Babel==0.8',
         'Flask-Login>=0.1.2',
         'Flask-OpenID>=1.1.0',
