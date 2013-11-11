e9f01b2f2c2a38b207b3b4d6b6897a258f8c7107
dpgaspar <danielvazgaspar@gmail.com>
2013-11-11 13:24:49 +0000

    Delete db_upgrade.py

--- db_upgrade.py
+++
@@ -1,6 +0,0 @@
-#!flask/bin/python
-from migrate.versioning import api
-from config import SQLALCHEMY_DATABASE_URI
-from config import SQLALCHEMY_MIGRATE_REPO
-api.upgrade(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
-print 'Current database version: ' + str(api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO))
