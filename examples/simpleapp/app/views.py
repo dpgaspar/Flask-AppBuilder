09358589cf1446fdec4022447f2055f5f26b8466
dpgaspar <danielvazgaspar@gmail.com>
2013-11-11 15:16:03 +0000

    static correction try1

--- examples/simpleapp/app/views.py
+++ examples/simpleapp/app/views.py
@@ -3,8 +3,22 @@ from flask.ext.appbuilder.baseapp import BaseApp
 from flask.ext.appbuilder.views import GeneralView
 from flask.ext.appbuilder.filters import *
 
+
+def debug_rules():
+    for rule in app.url_map.iter_rules():
+
+        options = {}
+        
+        methods = ','.join(rule.methods)
+	print "--------------------------------------------"
+        print "EP: %s METH: %s" % (rule.endpoint, methods)
+        print rule, rule.arguments
+	print "--------------------------------------------"
+
+
 menu = Menu()
 
 menu.add_link(name="Google", href="http://www.google.com",icon="",parent_category="External Links")
 baseapp = BaseApp(app, menu)
 
+debug_rules()
