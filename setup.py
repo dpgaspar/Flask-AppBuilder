a42b9627fd243a167d841139dd86b062238ef86e
dpgaspar <danielvazgaspar@gmail.com>
2013-11-12 13:51:52 +0000

    static

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Flask-AppBuilder',
-    version='0.0.43',
+    version='0.0.45',
     url='https://github.com/dpgaspar/flask-appbuilder/',
     license='BSD',
     author='Daniel Vaz Gaspar',
