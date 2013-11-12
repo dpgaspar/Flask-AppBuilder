6f152af1b51ce6b0e5747c2c311ad4466f0b1146
dpgaspar <danielvazgaspar@gmail.com>
2013-11-12 11:15:18 +0000

    static

--- flask_appbuilder/views.py
+++ flask_appbuilder/views.py
@@ -65,7 +65,7 @@ class BaseView(object):
         # Create blueprint and register rules
             self.blueprint = Blueprint(self.endpoint, __name__,
                                    url_prefix=self.route_base,
-                                   template_folder=self.template_folder)
+                                   template_folder=self.template_folder, static_folder='static')
         else:
             self.blueprint = Blueprint(self.endpoint, __name__,
                                    url_prefix=self.route_base,
@@ -438,7 +438,6 @@ class GeneralView(BaseCRUDView):
     @expose('/list/', methods=['GET', 'POST'])
     @has_access
     def list(self):
-	print 'URL STATIC', url_for('admin.static', filename='img/aol.png')
         form = self.search_form.refresh()
         search_form = self.search_form(request.form)
 
