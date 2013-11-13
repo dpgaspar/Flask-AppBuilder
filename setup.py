2ea4b51d3c88c6ee8db8a5bb0718422721cffcb8
dpgaspar <danielvazgaspar@gmail.com>
2013-11-13 16:00:30 +0000

    img correction

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def desc():
 
 setup(
     name='Flask-AppBuilder',
-    version='0.1.25',
+    version='0.1.26',
     url='https://github.com/dpgaspar/flask-appbuilder/',
     license='BSD',
     author='Daniel Vaz Gaspar',
