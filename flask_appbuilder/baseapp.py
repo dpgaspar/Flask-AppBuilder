5b378eff4947dcc106f2db7bf9dda4ea48ccac3b
dpgaspar <danielvazgaspar@gmail.com>
2013-11-14 17:52:03 +0000

    filters new method

--- flask_appbuilder/baseapp.py
+++ flask_appbuilder/baseapp.py
@@ -44,6 +44,7 @@ class BaseApp():
     
     def add_global_filters(self):
         self.template_filters = TemplateFilters(self.app)
+	print self.app.jinja_env.filters
 
     def add_global_static(self):
         bp = Blueprint('baseapp', __name__, url_prefix='/static',
