2ec58f994c6a23c019139626598e8fdabebe4175
dpgaspar <danielvazgaspar@gmail.com>
2013-11-14 13:32:12 +0000

    Update views.py

--- skeleton/app/views.py
+++ skeleton/app/views.py
@@ -1,11 +1,9 @@
-from flask.ext.appbuilder.menu import Menu
 from flask.ext.appbuilder.baseapp import BaseApp
 from flask.ext.appbuilder.views import GeneralView
 from flask.ext.appbuilder.filters import *
 
 
 
-menu = Menu()
 
-baseapp = BaseApp(app, menu)
+baseapp = BaseApp(app)
 
