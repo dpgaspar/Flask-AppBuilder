208d6ed2d1cc874b203ef4d685e285edc665e266
dpgaspar <danielvazgaspar@gmail.com>
2013-11-20 14:19:11 +0000

    Update datamodel.py

--- flask_appbuilder/models/datamodel.py
+++ flask_appbuilder/models/datamodel.py
@@ -84,6 +84,7 @@ class SQLAModel(DataModel):
         count = query_count.scalar() 
         
         if page: 
+            print "OFFSET", page*page_size
             query = query.offset(page*page_size)
         if page_size:
             query = query.limit(page_size)
