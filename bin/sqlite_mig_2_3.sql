9772297d523196ba3ac95ba00c118611b57f26ef
dpgaspar <danielvazgaspar@gmail.com>
2013-12-03 13:48:58 +0000

    Create sqlite_mig_2_3.sql

---
+++ bin/sqlite_mig_2_3.sql
@@ -0,0 +1,6 @@
+alter table user rename to ab_user;
+alter table role rename to ab_role;
+alter table permission rename to ab_permission;
+alter table permission_view rename to ab_permission_view;
+alter table permission_view_role rename to ab_permission_view_role;
+alter table view_menu rename to ab_view_menu;
