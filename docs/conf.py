7b64d264370795b2dfaa30d7022d3352229f277d
dpgaspar <danielvazgaspar@gmail.com>
2013-12-07 23:04:10 +0000

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ import os
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('..'))
-import flask_appbuilder
+#import flask_appbuilder
 
 # -- General configuration ------------------------------------------------
 
