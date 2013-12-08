e7c514ab4019b547c8967684e89fbc19230f9830
dpgaspar <danielvazgaspar@gmail.com>
2013-12-08 21:16:33 +0000

    docs theme change

--- docs/conf.py
+++ docs/conf.py
@@ -100,10 +100,13 @@ pygments_style = 'sphinx'
 
 
 # -- Options for HTML output ----------------------------------------------
+sys.path.append(os.path.abspath('_themes'))
+html_theme_path = ['_themes']
+html_theme = 'flask'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+#html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
