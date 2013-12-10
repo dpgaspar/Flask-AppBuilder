397507e462a96205f23e1b93a6bac1744298b9c1
dpgaspar <danielvazgaspar@gmail.com>
2013-12-10 16:57:36 +0000

    Update models.py

--- flask_appbuilder/security/models.py
+++ flask_appbuilder/security/models.py
@@ -30,7 +30,7 @@ class PermissionView(Base):
     __tablename__ = 'ab_permission_view'
     id = Column(Integer, primary_key=True)
     permission_id = Column(Integer, ForeignKey('ab_permission.id'))
-    permission = relationship("Permission", backref=backref('permissionview'))
+    permission = relationship("Permission")
     view_menu_id = Column(Integer, ForeignKey('ab_view_menu.id'))
     view_menu = relationship("ViewMenu")
 
