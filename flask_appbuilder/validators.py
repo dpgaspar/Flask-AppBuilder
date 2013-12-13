1661947cc8c4a07ee1c1b98e568bab62f2c4a86d
dpgaspar <danielvazgaspar@gmail.com>
2013-12-13 23:32:25 +0000

    Update validators.py

--- flask_appbuilder/validators.py
+++ flask_appbuilder/validators.py
@@ -4,16 +4,17 @@ from wtforms import ValidationError
 
 
 class Unique(object):
-    """Checks field value unicity against specified table field.
+    """
+        Checks field value unicity against specified table field.
 
-    :param get_session:
-        A function that return a SQAlchemy Session.
-    :param model:
-        The model to check unicity against.
-    :param column:
-        The unique column.
-    :param message:
-        The error message.
+        :param get_session:
+            A function that return a SQAlchemy Session.
+        :param model:
+            The model to check unicity against.
+        :param column:
+            The unique column.
+        :param message:
+            The error message.
     """
     field_flags = ('unique', )
 
@@ -29,8 +30,9 @@ class Unique(object):
                    .filter(self.column == field.data)
                    .one())
 
-            if self.message is None:
-                self.message = field.gettext(u'Already exists.')
-            raise ValidationError(self.message)
+            if not hasattr(form, '_obj') or not form._obj == obj:
+                if self.message is None:
+                    self.message = field.gettext(u'Already exists.')
+                raise ValidationError(self.message)
         except NoResultFound:
             pass
