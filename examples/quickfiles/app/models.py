7ec9e9a7f86c1e880b019c164fb6d095daa1ed1b
dpgaspar <danielvazgaspar@gmail.com>
2013-12-13 14:46:16 +0000

    Update models.py

--- examples/quickfiles/app/models.py
+++ examples/quickfiles/app/models.py
@@ -1,9 +1,10 @@
-
 from flask_appbuilder.models.mixins import AuditMixin, BaseMixin, FileColumn, ImageColumn
 from sqlalchemy import Table, Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
 from flask_appbuilder import Base
 from flask_appbuilder.security.models import User
-from app import db
+from flask_appbuilde import Base
+
 """
 
 You can use the extra Flask-AppBuilder fields and Mixin's
@@ -12,14 +13,14 @@ AuditMixin will add automatic timestamp of created and modified by who
 
 
 """
-        
-class Project(AuditMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name =  db.Column(db.String(150), unique = True, nullable=False)
+
+class Project(AuditMixin, Base):
+    id = Column(Integer, primary_key=True)
+    name =  Column(String(150), unique = True, nullable=False)
     
-class ProjectFiles(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    project_id = db.Column(db.Integer, db.ForeignKey('project.id'))
-    project = db.relationship("Project")
-    file = db.Column(FileColumn, nullable=False)
+class ProjectFiles(Base):
+    id = Column(Integer, primary_key=True)
+    project_id = Column(Integer, ForeignKey('project.id'))
+    project = relationship("Project")
+    file = Column(FileColumn, nullable=False)
     
