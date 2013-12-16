19ab91f5ff36ecd9b0c517df8a5de6104131b24a
dpgaspar <danielvazgaspar@gmail.com>
2013-12-16 17:15:57 +0000

    Update models.py

--- examples/quickimages/app/models.py
+++ examples/quickimages/app/models.py
@@ -1,40 +1,41 @@
 import datetime
 from flask import Markup
-from hashlib import md5
+from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
 from app import db
 from flask.ext.appbuilder.models.mixins import AuditMixin, BaseMixin, FileColumn, ImageColumn
 from flask.ext.appbuilder.filemanager import ImageManager
+from flask.ext.appbuilder import Base
 
-
-class Group(BaseMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name =  db.Column(db.String(50), unique = True, nullable=False)
-    address =  db.Column(db.String(264))
-    phone1 = db.Column(db.String(20))
-    phone2 = db.Column(db.String(20))
-    taxid = db.Column(db.Integer)
-    notes = db.Column(db.Text())
+class Group(BaseMixin, Base):
+    id = Column(Integer, primary_key=True)
+    name =  Column(String(50), unique = True, nullable=False)
+    address =  Column(String(264))
+    phone1 = Column(String(20))
+    phone2 = Column(String(20))
+    taxid = Column(Integer)
+    notes = Column(Text())
 
     def __repr__(self):
         return self.name
 
 
-class Person(BaseMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name =  db.Column(db.String(150), unique = True, nullable=False)
-    address =  db.Column(db.String(564))
-    birthday = db.Column(db.Date)
-    photo = db.Column(ImageColumn, nullable=False )
-    personal_phone = db.Column(db.String(20))
-    personal_celphone = db.Column(db.String(20))
-    personal_email = db.Column(db.String(64))
-    notes = db.Column(db.Text())
-    business_function = db.Column(db.String(64))
-    business_phone = db.Column(db.String(20))
-    business_celphone = db.Column(db.String(20))
-    business_email = db.Column(db.String(64))
-    group_id = db.Column(db.Integer, db.ForeignKey('group.id'))
-    group = db.relationship("Group")
+class Person(BaseMixin, Base):
+    id = Column(Integer, primary_key=True)
+    name =  Column(String(150), unique = True, nullable=False)
+    address =  Column(String(564))
+    birthday = Column(Date)
+    photo = Column(ImageColumn, nullable=False )
+    personal_phone = Column(String(20))
+    personal_celphone = Column(String(20))
+    personal_email = Column(String(64))
+    notes = Column(Text())
+    business_function = Column(String(64))
+    business_phone = Column(String(20))
+    business_celphone = Column(String(20))
+    business_email = Column(String(64))
+    group_id = Column(Integer, ForeignKey('group.id'))
+    group = relationship("Group")
 
 
     def photo_img(self):
