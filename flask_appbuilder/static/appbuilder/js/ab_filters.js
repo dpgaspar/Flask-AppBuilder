8189c3710c75c4d706c42328c0aeb7206af9ff77
dpgaspar <danielvazgaspar@gmail.com>
2013-12-27 16:35:12 +0000

    Update ab_filters.js

--- flask_appbuilder/static/appbuilder/js/ab_filters.js
+++ flask_appbuilder/static/appbuilder/js/ab_filters.js
@@ -1,4 +1,22 @@
 
-$('a.filter').click(function() {
-        alert('click');
+var AdminFilters = function(element, search_filters) {
+    var $root = $(element);
+    var $container = $('.filters', $root);
+    var lastCount = 0;
+
+
+    function addFilter(name, filter) {
+        var $el = $('<tr />').appendTo($container);
+
+        // Filter list
+        $el.append(name);
+    };
+
+    $('a.filter').click(function() {
+        var name = $(this).text().trim();
+        alert(name);
+        addFilter(name, search_filters[name]);
+        
     });
+
+};
