2efbeea0514c5b0534b32f7904fbc8b1e6e10d76
dpgaspar <danielvazgaspar@gmail.com>
2014-01-02 13:57:30 +0000

    Update ab_filters.js

--- flask_appbuilder/static/appbuilder/js/ab_filters.js
+++ flask_appbuilder/static/appbuilder/js/ab_filters.js
@@ -90,7 +90,7 @@ var AdminFilters = function(element, labels, form, filters, active_filters) {
 
         addFilterOptionsValue($el, name);
 	var $field1 = $(form[name])
-	var $field = $("input", $field1)
+	var $field = $("[@name=" + name", $field1)
 	$field.attr('name', '_flt_0_' + name);
 	$field.attr('class', ' filter_val ' + $field.attr('class'));
 	$el.append(
