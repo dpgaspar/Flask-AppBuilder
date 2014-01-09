65ddb204f9a16446b9db53387122990eb2de6bc6
dpgaspar <danielvazgaspar@gmail.com>
2014-01-09 12:30:38 +0000

    babel change

--- flask_appbuilder/babel.py
+++
@@ -1,9 +0,0 @@
-from flask.ext.babelex import Domain
-
-from flask.ext.appbuilder import translations
-
-domain = CustomDomain(dirname=translations.__path__[0])
-    
-gettext = domain.gettext
-ngettext = domain.ngettext
-lazy_gettext = domain.lazy_gettext
