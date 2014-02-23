e8637601f6783d85164e08e4c2ba830b9ffde0e5
dpgaspar <danielvazgaspar@gmail.com>
2014-02-23 14:51:14 +0000

    complex charts

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def desc():
 
 setup(
     name='Flask-AppBuilder',
-    version='0.6.9',
+    version='0.6.10',
     url='https://github.com/dpgaspar/flask-appbuilder/',
     license='BSD',
     author='Daniel Vaz Gaspar',
