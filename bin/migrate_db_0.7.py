693e4aee2c8489cf284a34218ed7aa8883ba7db5
dpgaspar <danielvazgaspar@gmail.com>
2014-03-07 10:48:28 +0000

    migration script

--- bin/migrate_db_0.7.py
+++ bin/migrate_db_0.7.py
@@ -4,16 +4,15 @@ from flask import Flask
 from sqlalchemy import create_engine
 from flask_appbuilder.security.models import User
 
-logging.basicConfig(format='%(asctime)s:%(levelname)s:%(name)s:%(message)s')
+logging.basicConfig(format='%(levelname)s:%(name)s:%(message)s')
 logging.getLogger().setLevel(logging.DEBUG)
-log = logging.getLogger(__name__)
+log = logging.getLogger('Database Migration to 0.7')
 
 try:
     app = Flask(__name__)
     app.config.from_object('config')
 
 except Exception as e:
-    from flask.ext.sqlalchemy import SQLAlchemy
 
     if len(sys.argv) < 2:
         print "Without typical app structure use parameter to config"
@@ -24,27 +23,40 @@ except Exception as e:
     app.config['SQLALCHEMY_DATABASE_URI'] = con_str
 
 
-def add_column(engine, table, column):
+add_column_stmt = {'mysql': 'ALTER TABLE %s ADD COLUMN %s %s',
+                   'sqlite': 'ALTER TABLE %s ADD COLUMN %s %s',
+                   'posgres': 'ALTER TABLE %s ADD COLUMN %s %s'}
+
+mod_column_stmt = {'mysql': 'ALTER TABLE %s ALTER COLUMN %s TYPE %s',
+                   'sqlite': 'ALTER TABLE %s ALTER COLUMN %s TYPE %s',
+                   'posgres': 'ALTER TABLE %s ALTER COLUMN %s TYPE %s'}
+
+
+
+def add_column(conn, table, column):
     table_name = table.__tablename__
     column_name = column.key
-    column_type = column.type.compile(engine.dialect)
+    column_type = column.type.compile(conn.dialect)
     try:
         log.info("Going to alter Column {0} on {1}".format(column_name, table_name))
-        engine.execute('ALTER TABLE %s ADD COLUMN %s %s' % (table_name, column_name, column_type))
+        conn.execute(add_column_stmt[conn.engine.name] % (table_name, column_name, column_type))
         log.info("Added Column {0} on {1}".format(column_name, table_name))
     except Exception as e:
         log.error("Error adding Column {0} on {1}: {2}".format(column_name, table_name, str(e)))
 
-def alter_column(engine, table, column):
+
+def alter_column(conn, table, column):
     table_name = table.__tablename__
     column_name = column.key
-    column_type = column.type.compile(engine.dialect)
+    column_type = column.type.compile(conn.dialect)
+
+    log.info("Going to alter Column {0} on {1}".format(column_name, table_name))
     try:
-	log.info("Going to alter Column {0} on {1}".format(column_name, table_name))
-        engine.execute('ALTER TABLE %s ALTER COLUMN %s TYPE %s' % (table_name, column_name, column_type))
+        conn.execute(mod_column_stmt[conn.engine.name] % (table_name, column_name, column_type))
         log.info("Altered Column {0} on {1}".format(column_name, table_name))
     except Exception as e:
         log.error("Error altering Column {0} on {1}: {2}".format(column_name, table_name, str(e)))
+
 
 engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
 con = engine.connect()
--- examples/quickhowto/migrate_db_0.7.py
+++ examples/quickhowto/migrate_db_0.7.py
@@ -4,48 +4,58 @@ from flask import Flask
 from sqlalchemy import create_engine
 from flask_appbuilder.security.models import User
 
-logging.basicConfig(format='%(asctime)s:%(levelname)s:%(name)s:%(message)s')
+logging.basicConfig(format='%(levelname)s:%(name)s:%(message)s')
 logging.getLogger().setLevel(logging.DEBUG)
-log = logging.getLogger(__name__)
+log = logging.getLogger('Database Migration to 0.7')
 
 try:
     app = Flask(__name__)
     app.config.from_object('config')
 
 except Exception as e:
-    from flask.ext.sqlalchemy import SQLAlchemy
 
-    log.error(str(e))
     if len(sys.argv) < 2:
         print "Without typical app structure use parameter to config"
-        print "Use example: python hash_db_password.py sqlite:////home/user/application/app.db"
+        print "Use example for sqlite: python migrate_db_0.7.py sqlite:////home/user/application/app.db"
         exit()
     con_str = sys.argv[1]
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = con_str
 
+add_column_stmt = {'mysql': 'ALTER TABLE %s ADD COLUMN %s %s',
+                   'sqlite': 'ALTER TABLE %s ADD COLUMN %s %s',
+                   'posgres': 'ALTER TABLE %s ADD COLUMN %s %s'}
 
-def add_column(engine, table, column):
+mod_column_stmt = {'mysql': 'ALTER TABLE %s ALTER COLUMN %s TYPE %s',
+                   'sqlite': 'ALTER TABLE %s ALTER COLUMN %s TYPE %s',
+                   'posgres': 'ALTER TABLE %s ALTER COLUMN %s TYPE %s'}
+
+
+
+def add_column(conn, table, column):
     table_name = table.__tablename__
     column_name = column.key
-    column_type = column.type.compile(engine.dialect)
+    column_type = column.type.compile(conn.dialect)
     try:
         log.info("Going to alter Column {0} on {1}".format(column_name, table_name))
-        engine.execute('ALTER TABLE %s ADD COLUMN %s %s' % (table_name, column_name, column_type))
+        conn.execute(add_column_stmt[conn.engine.name] % (table_name, column_name, column_type))
         log.info("Added Column {0} on {1}".format(column_name, table_name))
     except Exception as e:
         log.error("Error adding Column {0} on {1}: {2}".format(column_name, table_name, str(e)))
 
-def alter_column(engine, table, column):
+
+def alter_column(conn, table, column):
     table_name = table.__tablename__
     column_name = column.key
-    column_type = column.type.compile(engine.dialect)
+    column_type = column.type.compile(conn.dialect)
+
+    log.info("Going to alter Column {0} on {1}".format(column_name, table_name))
     try:
-	log.info("Going to alter Column {0} on {1}".format(column_name, table_name))
-        engine.execute('ALTER TABLE %s ALTER COLUMN %s TYPE %s' % (table_name, column_name, column_type))
+        conn.execute(mod_column_stmt[conn.engine.name] % (table_name, column_name, column_type))
         log.info("Altered Column {0} on {1}".format(column_name, table_name))
     except Exception as e:
         log.error("Error altering Column {0} on {1}: {2}".format(column_name, table_name, str(e)))
+
 
 engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
 con = engine.connect()
