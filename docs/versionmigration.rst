37db2b3f631202ad945f46f14db410efb0d25d34
dpgaspar <danielvazgaspar@gmail.com>
2014-03-17 23:00:10 +0000

    docs

--- docs/versionmigration.rst
+++ docs/versionmigration.rst
@@ -6,20 +6,49 @@ Migrating from 0.6.X to 0.7.X
 
 This new version has some breaking features. You don't have to change any code, main breaking changes are:
 
- - The security models schema has changed.
+ - The security models schema have changed.
 
- If you are using sqlite, mysql or pgsql, F.A.B. will alter the schema for you. If not, you will have to alter the schema your self. issue the corresponding DDL commands to:
+    If you are using sqlite, mysql or pgsql, use the following procedure:
 
- ALTER TABLE ab_user MODIFY COLUMN password VARCHAR(256)
- ALTER TABLE ab_user ADD COLUMN login_count INTEGER
- ALTER TABLE ab_user ADD COLUMN created_on DATETIME
- ALTER TABLE ab_user ADD COLUMN changed_on DATETIME
- ALTER TABLE ab_user ADD COLUMN created_by_fk INTEGER
- ALTER TABLE ab_user ADD COLUMN changed_by_fk INTEGER
- ALTER TABLE ab_user ADD COLUMN last_login DATETIME
- ALTER TABLE ab_user ADD COLUMN fail_login_count INTEGER
+        1 - Backup your DB.
+        2 - If you haven't already, upgrade to flask-appbuilder 0.7.0.
+        3 - Issue the following commands, on your project folder where config.py exists::
 
- - All passwords are kept on the database hashed, so all your passwords will be hashed automatically by the framework.
+            cd /your-main-project-folder/
+            wget https://raw.github.com/dpgaspar/Flask-AppBuilder/master/bin/migrate_db_0.7.py
+            python migrate_db_0.7.py
+            wget https://raw.github.com/dpgaspar/Flask-AppBuilder/master/bin/hash_db_password.py
+            python hash_db_password.py
+
+        4 - Test and Run (if you have a run.py for development) ::
+
+            python run.py
+
+    If not (DB is not sqlite, mysql or pgsql), you will have to alter the schema your self. use the following procedure:
+
+        1 - Backup your DB.
+        2 - If you haven't already, upgrade to flask-appbuilder 0.7.0.
+        3 - issue the corresponding DDL commands to:
+
+        ALTER TABLE ab_user MODIFY COLUMN password VARCHAR(256)
+        ALTER TABLE ab_user ADD COLUMN login_count INTEGER
+        ALTER TABLE ab_user ADD COLUMN created_on DATETIME
+        ALTER TABLE ab_user ADD COLUMN changed_on DATETIME
+        ALTER TABLE ab_user ADD COLUMN created_by_fk INTEGER
+        ALTER TABLE ab_user ADD COLUMN changed_by_fk INTEGER
+        ALTER TABLE ab_user ADD COLUMN last_login DATETIME
+        ALTER TABLE ab_user ADD COLUMN fail_login_count INTEGER
+
+        4 - Then hash your passwords::
+
+            wget https://raw.github.com/dpgaspar/Flask-AppBuilder/master/bin/hash_db_password.py
+            python hash_db_password.py
+
+ - All passwords are kept on the database hashed, so all your passwords will be hashed by the framework.
+
+ - Please *backup* your DB before altering the schema,  if you feel lost please post an issue on github
+    https://github.com/dpgaspar/Flask-AppBuilder/issues?state=open
+
 
 Migrating from 0.5.X to 0.6.X
 -----------------------------
---
+++ examples/quickhowto/migrate_db_0.7.py
@@ -0,0 +1,79 @@
+import sys
+import logging
+from flask import Flask
+from sqlalchemy import create_engine
+from flask_appbuilder.security.models import User
+
+logging.basicConfig(format='%(levelname)s:%(name)s:%(message)s')
+logging.getLogger().setLevel(logging.DEBUG)
+log = logging.getLogger('Database Migration to 0.7')
+
+try:
+    app = Flask(__name__)
+    app.config.from_object('config')
+
+except Exception as e:
+
+    if len(sys.argv) < 2:
+        print "Without typical app structure use parameter to config"
+        print "Use example for sqlite: python migrate_db_0.7.py sqlite:////home/user/application/app.db"
+        exit()
+    con_str = sys.argv[1]
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = con_str
+
+
+add_column_stmt = {'mysql': 'ALTER TABLE %s ADD COLUMN %s %s',
+                   'sqlite': 'ALTER TABLE %s ADD COLUMN %s %s',
+                   'postgresql': 'ALTER TABLE %s ADD COLUMN %s %s'}
+
+mod_column_stmt = {'mysql': 'ALTER TABLE %s MODIFY COLUMN %s %s',
+                   'sqlite': '',
+                   'postgresql': 'ALTER TABLE %s ALTER COLUMN %s TYPE %s'}
+
+
+def check_engine_support(conn):
+    if not conn.engine.name in add_column_stmt:
+        log.error('Engine type not supported by migration script, please alter schema for 0.7 read the documentation')
+        exit()
+
+def add_column(conn, table, column):
+    table_name = table.__tablename__
+    column_name = column.key
+    column_type = column.type.compile(conn.dialect)
+    try:
+        log.info("Going to alter Column {0} on {1}".format(column_name, table_name))
+        conn.execute(add_column_stmt[conn.engine.name] % (table_name, column_name, column_type))
+        log.info("Added Column {0} on {1}".format(column_name, table_name))
+    except Exception as e:
+        log.error("Error adding Column {0} on {1}: {2}".format(column_name, table_name, str(e)))
+
+
+def alter_column(conn, table, column):
+    table_name = table.__tablename__
+    column_name = column.key
+    column_type = column.type.compile(conn.dialect)
+
+    log.info("Going to alter Column {0} on {1}".format(column_name, table_name))
+    try:
+        conn.execute(mod_column_stmt[conn.engine.name] % (table_name, column_name, column_type))
+        log.info("Altered Column {0} on {1}".format(column_name, table_name))
+    except Exception as e:
+        log.error("Error altering Column {0} on {1}: {2}".format(column_name, table_name, str(e)))
+
+
+engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+conn = engine.connect()
+log.info("Database identified has {0}".format(conn.engine.name))
+check_engine_support(conn)
+
+alter_column(conn, User, User.password)
+add_column(conn, User, User.login_count)
+add_column(conn, User, User.created_on)
+add_column(conn, User, User.changed_on)
+add_column(conn, User, User.created_by_fk)
+add_column(conn, User, User.changed_by_fk)
+add_column(conn, User, User.last_login)
+add_column(conn, User, User.fail_login_count)
+
+conn.close()
