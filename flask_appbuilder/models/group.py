02bd41de3920f034bc7c47a14a41b253e668fc4c
dpgaspar <danielvazgaspar@gmail.com>
2014-04-11 12:17:47 +0100

    Update group.py

--- flask_appbuilder/models/group.py
+++ flask_appbuilder/models/group.py
@@ -120,7 +120,7 @@ class GroupByDateMonth(BaseGroupBy):
             return value.year, value.month 
 
     def get_format_group_col(self, item):
-        return calendar.month_name[item[0]] + ' ' + str(item[1])
+        return calendar.month_name[item[1]] + ' ' + str(item[0])
 
 
 class GroupBys(object):
