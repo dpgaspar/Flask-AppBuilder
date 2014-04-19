5db7ad341c06766ba1d75d3046e3d11633938e15
dpgaspar <danielvazgaspar@gmail.com>
2014-04-19 18:24:33 +0100

    translations

--- babel_extract.sh
+++ babel_extract.sh
@@ -1,4 +1,4 @@
-pybabel extract -F ./babel/babel.cfg -k lazy_gettext -o ./babel/messages.pot .
+pybabel extract -F ./babel/babel.cfg -k lazy_gettext -o ./babel/messages.pot flask_appbuilder
 pybabel update -i ./babel/messages.pot -d flask_appbuilder/translations
 pybabel compile -d flask_appbuilder/translations
 
