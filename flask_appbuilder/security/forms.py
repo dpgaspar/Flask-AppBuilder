d13a1810c5895b049f2d060c6b8feab6c85c1e93
dpgaspar <danielvazgaspar@gmail.com>
2014-04-21 22:29:26 +0100

    translations fix

--- flask_appbuilder/security/forms.py
+++ flask_appbuilder/security/forms.py
@@ -5,21 +5,24 @@ from flask.ext.wtf import Required, Length, validators, EqualTo
 from flask.ext.appbuilder.fieldwidgets import BS3PasswordFieldWidget
 from flask.ext.appbuilder.forms import DynamicForm
 
+
 class LoginForm_oid(DynamicForm):
-    openid = TextField(lazy_gettext('openid'), validators = [Required()])
-    remember_me = BooleanField(lazy_gettext('remember_me'), default = False)
+    openid = TextField(lazy_gettext('openid'), validators=[Required()])
+    remember_me = BooleanField(lazy_gettext('remember_me'), default=False)
+
 
 class LoginForm_db(DynamicForm):
-    username = TextField(lazy_gettext('User Name'), validators = [Required()])
-    password = PasswordField(lazy_gettext('Password'), validators = [Required()])
+    username = TextField(lazy_gettext('User Name'), validators=[Required()])
+    password = PasswordField(lazy_gettext('Password'), validators=[Required()])
 
 
 class ResetPasswordForm(DynamicForm):
     password = PasswordField(lazy_gettext('Password'),
-            description=lazy_gettext('Please use a good password policy, this application does not check this for you'),
-            validators = [Required()],
-            widget=BS3PasswordFieldWidget())
+                             description=lazy_gettext(
+                                 'Please use a good password policy, this application does not check this for you'),
+                             validators=[Required()],
+                             widget=BS3PasswordFieldWidget())
     conf_password = PasswordField(lazy_gettext('Confirm Password'),
-            description=lazy_gettext('Please rewrite the password to confirm'),
-            validators=[EqualTo('password', message=lazy_gettext('Passwords must match'))],
-            widget=BS3PasswordFieldWidget())
+                                  description=lazy_gettext('Please rewrite the password to confirm'),
+                                  validators=[EqualTo('password', message=lazy_gettext('Passwords must match'))],
+                                  widget=BS3PasswordFieldWidget())
