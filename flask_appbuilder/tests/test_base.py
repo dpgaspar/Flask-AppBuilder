a3929a0b79c3f6837028e005325ba3b206c763b4
dpgaspar <danielvazgaspar@gmail.com>
2014-04-28 12:43:49 +0100

    model and view tests

--- flask_appbuilder/tests/test_base.py
+++ flask_appbuilder/tests/test_base.py
@@ -19,10 +19,70 @@ def setup():
     db = SQLAlchemy(app)
     return db, app
 
+def create_models(db):
+    from sqlalchemy import Column, Integer, String, ForeignKey, Date
+    from sqlalchemy.orm import relationship
+    from flask.ext.appbuilder.models.mixins import BaseMixin
+    from flask.ext.appbuilder import Base
+
+    class Model1(BaseMixin, Base):
+        id = Column(Integer, primary_key=True)
+        field_string = Column(String(50), unique=True, nullable=False)
+        field_integer = Column(Integer())
+        field_date = Column(Date())
+
+        def __repr__(self):
+            return self.field_string
+
+    class Model2(BaseMixin, Base):
+        id = Column(Integer, primary_key=True)
+        field_string = Column(String(50), unique=True, nullable=False)
+        group_id = Column(Integer, ForeignKey('model1.id'), nullable=False)
+        group = relationship("Model1")
+
+        def __repr__(self):
+            return self.field_string
+
+
 
 def test_base_init():
+    """
+        Test F.A.B. initialization
+    """
     db, app = setup()
     genapp = BaseApp(app, db)
     ok_(len(genapp.baseviews) > 0) # current minimal views are 11
 
+
+def test_base_models():
+    """
+        Test F.A.B. Model creation
+    """
+    from sqlalchemy.engine.reflection import Inspector
+
+    db, app = setup()
+    create_models(db)
+    genapp = BaseApp(app, db)
+    engine = db.session.get_bind(mapper=None, clause=None)
+    inspector = Inspector.from_engine(engine)
+    ok_('model1' in inspector.get_table_names())
+    ok_('model2' in inspector.get_table_names())
+
+def test_base_views():
+    """
+        Test F.A.B. Basic views creation
+    """
+    from flask.ext.appbuilder.models.datamodel import SQLAModel
+    from flask.ext.appbuilder.views import GeneralView
+
+    db, app = setup()
+    create_models(db)
+    genapp = BaseApp(app, db)
+
+    class Model1View(GeneralView):
+        datamodel = SQLAModel(Model1, db.session)
+
+    class Model2View(GeneralView):
+        datamodel = SQLAModel(Model2, db.session)
+        related_views = [Model1View]
 
