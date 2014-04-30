c17feeb73d84d948ead94664c33ee252207c84cd
dpgaspar <danielvazgaspar@gmail.com>
2014-04-30 23:26:36 +0100

    tests init, crud, validations

--- flask_appbuilder/tests/test_base.py
+++ flask_appbuilder/tests/test_base.py
@@ -1,133 +1,222 @@
 from nose.tools import eq_, ok_, raises
+import unittest
+import os
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String, ForeignKey, Date
+from sqlalchemy.orm import relationship
+from flask.ext.appbuilder.models.mixins import BaseMixin
+from flask.ext.appbuilder import Base
+from flask.ext.appbuilder.baseapp import BaseApp
+
+from flask.ext.appbuilder.models.datamodel import SQLAModel
+from flask.ext.appbuilder.views import GeneralView
 
 import logging
 
-from . import setup
-
 DEFAULT_INDEX_STRING = 'Welcome'
+INVALID_LOGIN_STRING = 'Invalid login'
+UNIQUE_VALIDATION_STRING = 'Already exists'
+NOTNULL_VALIDATION_STRING = 'This field is required'
 
 log = logging.getLogger(__name__)
 
 
-def define_models():
-    from sqlalchemy import Column, Integer, String, ForeignKey, Date
-    from sqlalchemy.orm import relationship
-    from flask.ext.appbuilder.models.mixins import BaseMixin
-    from flask.ext.appbuilder import Base
+class Model1(BaseMixin, Base):
+    id = Column(Integer, primary_key=True)
+    field_string = Column(String(50), unique=True, nullable=False)
+    field_integer = Column(Integer())
+    field_date = Column(Date())
 
-    class Model1(BaseMixin, Base):
-        id = Column(Integer, primary_key=True)
-        field_string = Column(String(50), unique=True, nullable=False)
-        field_integer = Column(Integer())
-        field_date = Column(Date())
-
-        def __repr__(self):
-            return self.field_string
-
-    class Model2(BaseMixin, Base):
-        id = Column(Integer, primary_key=True)
-        field_string = Column(String(50), unique=True, nullable=False)
-        group_id = Column(Integer, ForeignKey('model1.id'), nullable=False)
-        group = relationship("Model1")
-
-        def __repr__(self):
-            return self.field_string
-
-    return Model1, Model2
+    def __repr__(self):
+        return self.field_string
 
 
-def setup_simple_app1(app, db):
-    from flask.ext.appbuilder.models.datamodel import SQLAModel
-    from flask.ext.appbuilder.views import GeneralView
-    from flask.ext.appbuilder.baseapp import BaseApp
+class Model2(BaseMixin, Base):
+    id = Column(Integer, primary_key=True)
+    field_string = Column(String(50), unique=True, nullable=False)
+    group_id = Column(Integer, ForeignKey('model1.id'), nullable=False)
+    group = relationship("Model1")
 
-    Model1, Model2 = define_models()
-    genapp = BaseApp(app, db)
-
-    class Model1View(GeneralView):
-        datamodel = SQLAModel(Model1, db.session)
-
-    class Model2View(GeneralView):
-        datamodel = SQLAModel(Model2, db.session)
-        related_views = [Model1View]
-
-    genapp.add_view(Model1View(), "Model1")
-    genapp.add_view(Model2View(), "Model2")
-    return genapp, Model1, Model2
+    def __repr__(self):
+        return self.field_string
 
 
-def test_base_init():
-    from flask.ext.appbuilder.baseapp import BaseApp
+class FlaskTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = Flask(__name__)
+        self.basedir = os.path.abspath(os.path.dirname(__file__))
+        self.app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'
+        self.app.config['CSRF_ENABLED'] = False
+        self.app.config['SECRET_KEY'] = 'thisismyscretkey'
+        self.app.config['WTF_CSRF_ENABLED'] = False
 
-    """
-        Test F.A.B. initialization
-    """
-    db, app = setup()
-    baseapp = BaseApp(app, db)
-    ok_(len(baseapp.baseviews) > 9) # current minimal views are 11
+        self.db = SQLAlchemy(self.app)
 
+        class Model1View(GeneralView):
+            datamodel = SQLAModel(Model1, self.db.session)
 
-def test_model_creation():
-    from flask.ext.appbuilder.baseapp import BaseApp
+        class Model2View(GeneralView):
+            datamodel = SQLAModel(Model2, self.db.session)
+            related_views = [Model1View]
 
-    """
-        Test F.A.B. Model creation
-    """
-    from sqlalchemy.engine.reflection import Inspector
-
-    db, app = setup()
-    define_models()
-    genapp = BaseApp(app, db)
-    engine = db.session.get_bind(mapper=None, clause=None)
-    inspector = Inspector.from_engine(engine)
-    # Check if tables exist
-    ok_('model1' in inspector.get_table_names())
-    ok_('model2' in inspector.get_table_names())
+        self.baseapp = BaseApp(self.app, self.db)
+        self.baseapp.add_view(Model1View(), "Model1")
+        self.baseapp.add_view(Model2View(), "Model2")
 
 
-def test_base_views():
-    """
-        Test F.A.B. Basic views creation
-    """
+    def tearDown(self):
+        log.debug("TEAR DOWN")
 
-    db, app = setup()
 
-    baseapp, Model1, Model2 = setup_simple_app1(app, db)
-    client = app.test_client()
-    
-    # Check for Welcome Message    
-    rv = client.get('/')
-    data = rv.data.decode('utf-8')
-    ok_(DEFAULT_INDEX_STRING in data)
-    
-    # Try List and Redirect to Login
-    rv = client.get('/model1view/list/')
-    eq_(rv.status_code, 302)
-    rv = client.get('/model2view/list/')
-    eq_(rv.status_code, 302)
+    def login(self, client, username, password):
+        # Login with default admin
+        return client.post('/login/', data=dict(
+            username=username,
+            password=password
+        ), follow_redirects=True)
 
-    # Login with default admin
-    client.post('/login/', data=dict(
-        username='admin',
-        password='general'
-    ), follow_redirects=True)
-    # Test access to authorized views
-    rv = client.get('/model1view/list/')
-    eq_(rv.status_code, 200)
-    rv = client.get('/model2view/list/')
-    eq_(rv.status_code, 200)
+    def logout(self, client):
+        return client.get('/logout/')
 
-    rv = client.get('/model1view/add')
-    eq_(rv.status_code, 200)
 
-    """
-    # Add one record and test if it's on the db
-    rv = client.post('/model1view/add?next=%2Fmodel1view%2Flist%2F',
-                     data=dict(field_string='test1', field_integer='1'))
-    eq_(rv.status_code, 200)
+    def test_fab_views(self):
+        """
+            Test views creation and registration
+        """
+        ok_(len(self.baseapp.baseviews) > 9)  # current minimal views are 11
 
-    model = db.session.query(Model1).first()
-    eq_(model.field_string, u'test1')
-    eq_(model.field_integer, 1)
-    """
 
+    def test_model_creation(self):
+        """
+            Test Model creation
+        """
+        from sqlalchemy.engine.reflection import Inspector
+
+        engine = self.db.session.get_bind(mapper=None, clause=None)
+        inspector = Inspector.from_engine(engine)
+        # Check if tables exist
+        ok_('model1' in inspector.get_table_names())
+        ok_('model2' in inspector.get_table_names())
+
+    def test_index(self):
+        """
+            Test initial access and index message
+        """
+        client = self.app.test_client()
+
+        # Check for Welcome Message
+        rv = client.get('/')
+        data = rv.data.decode('utf-8')
+        ok_(DEFAULT_INDEX_STRING in data)
+
+    def test_sec_login(self):
+        """
+            Test Security Login, Logout
+        """
+        client = self.app.test_client()
+
+        # Try to List and Redirect to Login
+        rv = client.get('/model1view/list/')
+        eq_(rv.status_code, 302)
+        rv = client.get('/model2view/list/')
+        eq_(rv.status_code, 302)
+
+        # Login and list with admin
+        self.login(client, 'admin', 'general')
+        rv = client.get('/model1view/list/')
+        eq_(rv.status_code, 200)
+        rv = client.get('/model2view/list/')
+        eq_(rv.status_code, 200)
+
+        # Logout and and try to list
+        self.logout(client)
+        rv = client.get('/model1view/list/')
+        eq_(rv.status_code, 302)
+        rv = client.get('/model2view/list/')
+        eq_(rv.status_code, 302)
+
+        # Invalid Login
+        rv = self.login(client, 'admin', 'badpassword')
+        data = rv.data.decode('utf-8')
+        ok_(INVALID_LOGIN_STRING in data)
+
+    def test_model_crud(self):
+        """
+            Test Model add, delete, edit
+        """
+        client = self.app.test_client()
+        self.login(client, 'admin', 'general')
+
+        rv = client.post('/model1view/add',
+                         data=dict(field_string='test1', field_integer='1'), follow_redirects=True)
+        eq_(rv.status_code, 200)
+
+        model = self.db.session.query(Model1).first()
+        eq_(model.field_string, u'test1')
+        eq_(model.field_integer, 1)
+
+        rv = client.post('/model1view/edit/1',
+                         data=dict(field_string='test2', field_integer='2'), follow_redirects=True)
+        eq_(rv.status_code, 200)
+
+        model = self.db.session.query(Model1).first()
+        eq_(model.field_string, u'test2')
+        eq_(model.field_integer, 2)
+
+        rv = client.get('/model1view/delete/1', follow_redirects=True)
+        eq_(rv.status_code, 200)
+        model = self.db.session.query(Model1).first()
+        eq_(model, None)
+
+    def test_model_add_validation(self):
+        """
+            Test Model add validations
+        """
+        client = self.app.test_client()
+        self.login(client, 'admin', 'general')
+
+        rv = client.post('/model1view/add',
+                         data=dict(field_string='test1', field_integer='1'), follow_redirects=True)
+        eq_(rv.status_code, 200)
+
+        rv = client.post('/model1view/add',
+                         data=dict(field_string='test1', field_integer='2'), follow_redirects=True)
+        eq_(rv.status_code, 200)
+        data = rv.data.decode('utf-8')
+        ok_(UNIQUE_VALIDATION_STRING in data)
+
+        model = self.db.session.query(Model1).all()
+        eq_(len(model), 1)
+
+        rv = client.post('/model1view/add',
+                         data=dict(field_string='', field_integer='1'), follow_redirects=True)
+        eq_(rv.status_code, 200)
+        data = rv.data.decode('utf-8')
+        ok_(NOTNULL_VALIDATION_STRING in data)
+
+        model = self.db.session.query(Model1).all()
+        eq_(len(model), 1)
+
+    def test_model_edit_validation(self):
+        """
+            Test Model edit validations
+        """
+        client = self.app.test_client()
+        self.login(client, 'admin', 'general')
+
+        client.post('/model1view/add',
+                         data=dict(field_string='test1', field_integer='1'), follow_redirects=True)
+        client.post('/model1view/add',
+                         data=dict(field_string='test2', field_integer='1'), follow_redirects=True)
+        rv = client.post('/model1view/edit/1',
+                         data=dict(field_string='test2', field_integer='2'), follow_redirects=True)
+        eq_(rv.status_code, 200)
+        data = rv.data.decode('utf-8')
+        ok_(UNIQUE_VALIDATION_STRING in data)
+
+        rv = client.post('/model1view/edit/1',
+                         data=dict(field_string='', field_integer='2'), follow_redirects=True)
+        eq_(rv.status_code, 200)
+        data = rv.data.decode('utf-8')
+        ok_(NOTNULL_VALIDATION_STRING in data)
