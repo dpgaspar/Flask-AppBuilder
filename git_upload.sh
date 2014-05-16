d5ba77404a1feefe566395794a3b5c4993d3d763
dpgaspar <danielvazgaspar@gmail.com>
2014-05-17 00:31:50 +0100

    cleanup

--- git_upload.sh
+++ git_upload.sh
@@ -1,23 +1,3 @@
-git add LICENSE
-git add *.py -A
-git add *.in -A
-git add *.cfg -A
-git add *.sh -A
-git add *.mo -A
-git add *.po -A
-git add .gitignore
-git add *.md -A
-git add *.rst -A
-git add *.txt -A
-git add *.png -A
-git add ./flask_appbuilder -A
-git add ./bin -A
-git add ./babel -A
-git add ./docs -A
-git add ./docs/_static -A
-git add ./examples -A
-git add ./tests -A
-git add .travis.yml
-git commit -m "$1"
+git commit -a -m "$1"
 git push origin $2
 
