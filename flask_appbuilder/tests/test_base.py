7d3e3f00a9701bf6fed24abe5acc143400303f30
dpgaspar <danielvazgaspar@gmail.com>
2014-06-09 11:35:04 +0100

    Update test_base.py

--- flask_appbuilder/tests/test_base.py
+++ flask_appbuilder/tests/test_base.py
@@ -430,7 +430,7 @@ class FlaskTestCase(unittest.TestCase):
         """
         client = self.app.test_client()
         self.login(client, DEFAULT_ADMIN_USER, DEFAULT_ADMIN_PASSWORD)
-        self.insert_data2()
+        #self.insert_data2()
         log.info("CHART TEST")
         rv = client.get('/model2chartview/chart/')
         eq_(rv.status_code, 200)
@@ -449,7 +449,7 @@ class FlaskTestCase(unittest.TestCase):
         """
         client = self.app.test_client()
         self.login(client, DEFAULT_ADMIN_USER, DEFAULT_ADMIN_PASSWORD)
-        self.insert_data2()
+        #self.insert_data2()
         rv = client.get('/model1masterview/list/')
         eq_(rv.status_code, 200)
         rv = client.get('/model1masterview/list/1')
