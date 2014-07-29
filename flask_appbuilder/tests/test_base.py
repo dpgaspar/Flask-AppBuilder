82e22448ab72e5dfb7ce5c1cf496ff1b82b1f418
dpgaspar <danielvazgaspar@gmail.com>
2014-07-29 18:43:17 +0100

    Update test_base.py

--- flask_appbuilder/tests/test_base.py
+++ flask_appbuilder/tests/test_base.py
@@ -387,6 +387,7 @@ class FlaskTestCase(unittest.TestCase):
         # Base filter string starts with
         rv = client.get('/model2view/edit')
         data = rv.data.decode('utf-8')
+        print data
         ok_('G2' in data)
         ok_('G1' not in data)
 
