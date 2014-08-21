2db4d2832ee6d0cc433668fef8c27d191218360d
dpgaspar <danielvazgaspar@gmail.com>
2014-08-21 12:26:17 +0100

    Update ab_filters.js

--- flask_appbuilder/static/appbuilder/js/ab_filters.js
+++ flask_appbuilder/static/appbuilder/js/ab_filters.js
@@ -39,8 +39,8 @@ var AdminFilters = function(element, labels, form, filters, active_filters) {
 		$field_inner.attr('class', ' filter_val ' + $field_inner.attr('class'));
 	}
 	else {
-		alert($field);
-		
+		alert($(field).attr( 'type'));
+		$(field).attr( 'checked', true );
 		$field.attr('name', '_flt_' + i_option + '_' + name);	
 		$field.val(value);
         	$field.attr('class', ' filter_val ' + $field.attr('class'));
