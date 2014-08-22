16c282ee2e4c1adad830572a8093551809db3cce
dpgaspar <danielvazgaspar@gmail.com>
2014-08-22 13:40:29 +0100

    Update models.py

--- examples/quickhowto/app/models.py
+++ examples/quickhowto/app/models.py
@@ -4,6 +4,8 @@ from sqlalchemy.orm import relationship
 from flask.ext.appbuilder.models.mixins import AuditMixin, BaseMixin, FileColumn, ImageColumn
 from flask.ext.appbuilder import Model
 
+mindate = datetime.date(datetime.MINYEAR, 1, 1)
+
 class Group(Model):
     id = Column(Integer, primary_key=True)
     name = Column(String(50), unique = True, nullable=False)
@@ -36,7 +38,10 @@ class Contact(Model):
         return self.name
 
     def month_year(self):
-        return datetime.datetime(self.birthday.year, self.birthday.month, 1)
+        date = self.birthday or mindate
+        return datetime.datetime(date.year, date.month, 1) or mindate
 
     def year(self):
-        return datetime.datetime(self.birthday.year, 1, 1)
+        date = self.birthday or mindate
+        return datetime.datetime(date.year, 1, 1)
+        
