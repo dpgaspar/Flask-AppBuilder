473841696699919cb33652dbc5a19d2bb1d09a07
dpgaspar <danielvazgaspar@gmail.com>
2015-01-07 16:21:45 +0000

    test correction

--- flask_appbuilder/tests/test_base.py
+++ flask_appbuilder/tests/test_base.py
@@ -99,7 +99,7 @@ class FlaskTestCase(unittest.TestCase):
 
             edit_form_query_rel_fields = {'group':[['field_string', FilterEqual, 'G2']]}
 
-            add_form_query_rel_fields = {'group':[['field_string', FilterEqual, 'G1']]
+            add_form_query_rel_fields = {'group':[['field_string', FilterEqual, 'G1']]}
 
         class Model1View(ModelView):
             datamodel = SQLAModel(Model1)
