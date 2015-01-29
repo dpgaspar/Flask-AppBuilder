bb540fbfd11729c030d4872c7e9780d3915c65b8
dpgaspar <danielvazgaspar@gmail.com>
2015-01-29 08:08:45 +0000

    MongoEngine with images

---
+++ examples/mongoimages/app/__init__.py
@@ -0,0 +1,18 @@
+import logging
+
+from flask import Flask
+
+from flask_appbuilder import AppBuilder
+from flask_appbuilder.security.mongoengine.manager import SecurityManager
+from flask_mongoengine import MongoEngine
+
+
+logging.basicConfig(format='%(asctime)s:%(levelname)s:%(name)s:%(message)s')
+logging.getLogger().setLevel(logging.DEBUG)
+
+app = Flask(__name__)
+app.config.from_object('config')
+dbmongo = MongoEngine(app)
+appbuilder = AppBuilder(app, security_manager_class=SecurityManager)
+
+from app import models, views
---
+++ examples/quicktemplates/app/views.py
@@ -0,0 +1,108 @@
+import calendar
+from flask.ext.appbuilder import ModelView
+from flask.ext.appbuilder.models.sqla.interface import SQLAInterface
+from flask.ext.appbuilder.charts.views import GroupByChartView
+from flask.ext.appbuilder.models.group import aggregate_count
+from flask.ext.babelpkg import lazy_gettext as _
+
+
+from app import db, appbuilder
+from .models import ContactGroup, Gender, Contact
+
+
+def fill_gender():
+    try:
+        db.session.add(Gender(name='Male'))
+        db.session.add(Gender(name='Female'))
+        db.session.commit()
+    except:
+        db.session.rollback()
+
+
+class ContactModelView(ModelView):
+    datamodel = SQLAInterface(Contact)
+
+    list_columns = ['name', 'personal_celphone', 'birthday', 'contact_group.name']
+
+    base_order = ('name', 'asc')
+
+    show_fieldsets = [
+        ('Summary', {'fields': ['name', 'gender', 'contact_group']}),
+        (
+            'Personal Info',
+            {'fields': ['address', 'birthday', 'personal_phone', 'personal_celphone'], 'expanded': False}),
+    ]
+
+    add_fieldsets = [
+        ('Summary', {'fields': ['name', 'gender', 'contact_group']}),
+        (
+            'Personal Info',
+            {'fields': ['address', 'birthday', 'personal_phone', 'personal_celphone'], 'expanded': False}),
+    ]
+
+    edit_fieldsets = [
+        ('Summary', {'fields': ['name', 'gender', 'contact_group']}),
+        (
+            'Personal Info',
+            {'fields': ['address', 'birthday', 'personal_phone', 'personal_celphone'], 'expanded': False}),
+    ]
+
+
+class GroupModelView(ModelView):
+    datamodel = SQLAInterface(ContactGroup)
+    related_views = [ContactModelView]
+
+
+class ContactChartView(GroupByChartView):
+    datamodel = SQLAInterface(Contact)
+    chart_title = 'Grouped contacts'
+    label_columns = ContactModelView.label_columns
+    chart_type = 'PieChart'
+
+    definitions = [
+        {
+            'group' : 'contact_group',
+            'series' : [(aggregate_count,'contact_group')]
+        },
+        {
+            'group' : 'gender',
+            'series' : [(aggregate_count,'contact_group')]
+        }
+    ]
+
+
+def pretty_month_year(value):
+    return calendar.month_name[value.month] + ' ' + str(value.year)
+
+def pretty_year(value):
+    return str(value.year)
+
+
+class ContactTimeChartView(GroupByChartView):
+    datamodel = SQLAInterface(Contact)
+
+    chart_title = 'Grouped Birth contacts'
+    chart_type = 'AreaChart'
+    label_columns = ContactModelView.label_columns
+    definitions = [
+        {
+            'group' : 'month_year',
+            'formatter': pretty_month_year,
+            'series': [(aggregate_count, 'group')]
+        },
+        {
+            'group': 'year',
+            'formatter': pretty_year,
+            'series': [(aggregate_count, 'group')]
+        }
+    ]
+
+
+db.create_all()
+fill_gender()
+appbuilder.add_view(GroupModelView, "List Groups", icon="fa-folder-open-o", category="Contacts", category_icon='fa-envelope')
+appbuilder.add_view(ContactModelView, "List Contacts", icon="fa-envelope", category="Contacts")
+appbuilder.add_separator("Contacts")
+appbuilder.add_view(ContactChartView, "Contacts Chart", icon="fa-dashboard", category="Contacts")
+appbuilder.add_view(ContactTimeChartView, "Contacts Birth Chart", icon="fa-dashboard", category="Contacts")
+
