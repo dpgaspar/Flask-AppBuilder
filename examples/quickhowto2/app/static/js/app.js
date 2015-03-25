6afed2aa89012f0477ba57109a409a3050a173e6
dpgaspar <danielvazgaspar@gmail.com>
2015-03-25 13:21:43 +0000

    angular exp

--- examples/quickhowto2/app/static/js/app.js
+++ examples/quickhowto2/app/static/js/app.js
@@ -5,6 +5,7 @@ app.config(['$interpolateProvider', function($interpolateProvider) {
   $interpolateProvider.endSymbol(']}');
 }]);
 
+
 app.factory("loadingManager", function($rootScope) {
   return {
     loading: function() {
@@ -15,6 +16,26 @@ app.factory("loadingManager", function($rootScope) {
     }
   };
 
+});
+
+
+app.factory("filterManager", function() {
+    return {
+        filters: {}, // TYPE: { 'MODELVIEW_NAME': [[COL, FILTER_TYPE, VALUE],[COL, FILTER_TYPE, VALUE]...], ... }
+
+        addFilter: function(modelview_name, col, filter_type, value) {
+            filter = [col, filter_type, value];
+            if (!filters[modelview_name]) {
+                filters[modelview_name] = [filter];
+            }
+            else {
+                filters[modelview_name].push(filter);
+            }
+        },
+        removeFilter: function(modelview_name, index) {
+            filters[modelview_name].splice(index, 1);
+        }
+    };
 });
 
 
