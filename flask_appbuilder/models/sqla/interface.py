ac3f37701cb3071fb7eefa599244abe09ad433eb
dpgaspar <danielvazgaspar@gmail.com>
2015-08-28 09:44:36 +0100

    Bug fixed for problem with columns that drilldown model.model.name

--- flask_appbuilder/models/sqla/interface.py
+++ flask_appbuilder/models/sqla/interface.py
@@ -61,8 +61,9 @@ class SQLAInterface(BaseInterface):
         if order_column != '':
             # if Model has custom decorator **renders('<COL_NAME>')**
             # this decorator will add a property to the method named *_col_name*
-            if hasattr(getattr(self.obj, order_column), '_col_name'):
-                order_column = getattr(getattr(self.obj, order_column),'_col_name')
+            if hasattr(self.obj, order_column):
+                if hasattr(getattr(self.obj, order_column), '_col_name'):
+                    order_column = getattr(getattr(self.obj, order_column),'_col_name')
             query = query.order_by(order_column + ' ' + order_direction)
         return query
 
