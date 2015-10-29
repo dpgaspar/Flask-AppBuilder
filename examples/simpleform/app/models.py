bd0e16d860c35616c0f78e51cb059a94c869b4bd
dpgaspar <danielvazgaspar@gmail.com>
2015-10-29 15:39:57 +0000

    added example for simple forms

---
+++ examples/simpleform/app/models.py
@@ -0,0 +1,13 @@
+from flask.ext.appbuilder import Model
+from flask.ext.appbuilder.models.mixins import AuditMixin, FileColumn, ImageColumn
+from sqlalchemy import Column, Integer, String, ForeignKey 
+from sqlalchemy.orm import relationship
+"""
+
+You can use the extra Flask-AppBuilder fields and Mixin's
+
+AuditMixin will add automatic timestamp of created and modified by who
+
+
+"""
+        
---
+++ examples/simpleform/app/views.py
@@ -0,0 +1,32 @@
+from flask import render_template, flash
+from flask_appbuilder import SimpleFormView
+from flask_babelpkg import lazy_gettext as _
+from .forms import MyForm
+from app import appbuilder, db
+
+
+class MyFormView(SimpleFormView):
+    form = MyForm
+    form_title = 'This is my first form view'
+    message = 'My form was submitted'
+
+    def form_get(self, form):
+        form.field1 = 'This was prefilled'
+
+    def form_post(self, form):
+        # post process form
+        flash(self.message, 'info')
+
+appbuilder.add_view(MyFormView, "My form View", icon="fa-group", label=_('My form View'),
+                     category="My Forms", category_icon="fa-cogs")
+
+"""
+    Application wide 404 error handler
+"""
+@appbuilder.app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html', base_template=appbuilder.base_template, appbuilder=appbuilder), 404
+
+db.create_all()
+
+
