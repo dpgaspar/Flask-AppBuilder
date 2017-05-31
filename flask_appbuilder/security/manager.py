cdfc46a0b25f4682372ea4d78ba4e31e37382d81
Matt Knox <matt.knox@sphero.com>
2017-05-31 09:08:33 -0600

    Google returns ID directly

--- flask_appbuilder/security/manager.py
+++ flask_appbuilder/security/manager.py
@@ -378,7 +378,7 @@ class BaseSecurityManager(AbstractSecurityManager):
         if provider == 'google':
             me = self.appbuilder.sm.oauth_remotes[provider].get('people/me')
             log.debug("User info from Google: {0}".format(me.data))
-            return {'username': "google_" + me.data['id'].get('value',''),
+            return {'username': "google_" + me.data['id'],
                 'email': me.data['emails'][0].get('value',''),
                 'first_name': me.data['name'].get('givenName',''),
                 'last_name': me.data['name'].get('familyName','')}
