2a08f21a2d6006c4850a1e6460abc2c00bfa871c
Andrew Otto <acotto@gmail.com>
2017-12-08 10:14:41 -0500

    Support AUTH_USER_REGISTRATION with AUTH_REMOTE_USER

--- flask_appbuilder/security/manager.py
+++ flask_appbuilder/security/manager.py
@@ -688,12 +688,28 @@ class BaseSecurityManager(AbstractSecurityManager):
             :type self: User model
         """
         user = self.find_user(username=username)
-        if user is None or (not user.is_active()):
+
+        # User does not exist, create one if auto user registration.
+        if user is None and self.auth_user_registration:
+            log.info(LOGMSG_WAR_SEC_LOGIN_FAILED.format(username))
+            user = self.add_user(
+                # All we have is REMOTE_USER, so we set
+                # the other fields to blank.
+                username=username,
+                first_name=username,
+                last_name='-',
+                email='-',
+                role=self.find_role(self.auth_user_registration_role)
+            )
+
+        # If user does not exist on the DB and not auto user registration,
+        # or user is inactive, go away.
+        elif user is None or (not user.is_active()):
             log.info(LOGMSG_WAR_SEC_LOGIN_FAILED.format(username))
             return None
-        else:
-            self.update_user_auth_stat(user)
-            return user
+
+        self.update_user_auth_stat(user)
+        return user
 
     def auth_user_oauth(self, userinfo):
         """
