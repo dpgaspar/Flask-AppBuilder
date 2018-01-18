0b047b1b7d9d49ab126316554fe6fb4739bf397a
Hunter-liu <15037889077@163.com>
2018-01-18 23:45:51 +0800

    Update manager.py

--- flask_appbuilder/security/manager.py
+++ flask_appbuilder/security/manager.py
@@ -399,7 +399,6 @@ class BaseSecurityManager(AbstractSecurityManager):
         # for weixin
         if provider == 'weixin':
             me = self.appbuilder.sm.oauth_remotes[provider].get('me')
-            log.debug("User info from qiye-wechat: {0}".format(me.data))
             return {'username': me.data.get('userid', '') + "_" +me.data.get('name', ''),
                 #'first_name': me.data.get('name', ''),
                 #'last_name': me.data.get('family_name', ''),
