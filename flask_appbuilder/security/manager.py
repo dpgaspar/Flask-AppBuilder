0612264c083c53d2159cb564926c876d7b760dba
Shinnakrit Jirasopon <sjiras@sertiscorp.com>
2018-02-08 15:42:11 +0700

    Comment Log

--- flask_appbuilder/security/manager.py
+++ flask_appbuilder/security/manager.py
@@ -255,7 +255,7 @@ class BaseSecurityManager(AbstractSecurityManager):
             now = current_time()
             for key in redis_client.hgetall(app.config['REDIS_KEY']).keys():
                 idle_time = now - int(redis_client.hget(app.config['REDIS_KEY'], key))
-                print_idle_time(key, idle_time)
+                # print_idle_time(key, idle_time)
                 if idle_time > (app.config['REMEMBER_COOKIE_DURATION'].total_seconds() * 1000):
                     user = self.get_user_by_id(key)
                     print('User "' + str(user.email) + '" logged off.')
