7f1df9268397dc2f717129bc87542c87eea820b7
Dolf Andringa <dolfandringa@gmail.com>
2018-10-06 00:42:10 +0800

    Fix issue #820 by implementing SQLAInterface.is_relation_one_to_one properly

--- flask_appbuilder/tests/test_sqlalchemy.py
+++ flask_appbuilder/tests/test_sqlalchemy.py
@@ -2,6 +2,31 @@ from nose.tools import eq_
 import unittest
 import sqlalchemy as sa
 from flask_appbuilder.models.sqla.interface import _is_sqla_type
+from flask_appbuilder.models.sqla.interface import SQLAInterface
+
+Base = sa.ext.declarative.declarative_base()
+
+
+class Parent(Base):
+    __tablename__ = 'parent'
+    id = sa.Column(sa.Integer, primary_key=True)
+    favorite_child = sa.orm.relation('FavoriteChild', back_populates='parent',
+                                     uselist=False)
+    children = sa.orm.relation('Child', back_populates='parent')
+
+
+class FavoriteChild(Base):
+    __tablename__ = 'favorite_child'
+    id = sa.Column(sa.Integer, primary_key=True)
+    parent_id = sa.Column(sa.Integer, sa.ForeignKey('parent.id'))
+    parent = sa.orm.relation(Parent, back_populates='favorite_child')
+
+
+class Child(Base):
+    __tablename__ = 'child'
+    id = sa.Column(sa.Integer, primary_key=True)
+    parent_id = sa.Column(sa.Integer, sa.ForeignKey('parent.id'))
+    parent = sa.orm.relation(Parent, back_populates='children')
 
 
 class CustomSqlaType(sa.types.TypeDecorator):
@@ -14,6 +39,16 @@ class NotSqlaType():
 
 
 class FlaskTestCase(unittest.TestCase):
+    def test_is_one_to_one(self):
+        interf = SQLAInterface(Parent)
+        eq_(True,  interf.is_relation_one_to_one('favorite_child'))
+        eq_(False,  interf.is_relation_one_to_many('favorite_child'))
+
+    def test_is_one_to_many(self):
+        interf = SQLAInterface(Parent)
+        eq_(True,  interf.is_relation_one_to_many('children'))
+        eq_(False,  interf.is_relation_one_to_one('children'))
+
     def test_is_sqla_type(self):
         t1 = sa.types.DateTime(timezone=True)
         t2 = CustomSqlaType()
