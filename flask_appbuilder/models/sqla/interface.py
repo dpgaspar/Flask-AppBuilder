f4bb369d41e9a8f34425bbea918f41c05f169d5d
Denke Ákos <denke.akos@vtvsystems.hu>
2018-12-31 11:44:46 +0100

    use model relationship on column ordering

--- flask_appbuilder/models/sqla/interface.py
+++ flask_appbuilder/models/sqla/interface.py
@@ -95,14 +95,9 @@ class SQLAInterface(BaseInterface):
         """
         query = self.session.query(self.obj)
         if len(order_column.split('.')) >= 2:
-            tmp_order_column = ''
             for join_relation in order_column.split('.')[:-1]:
-                model_relation = self.get_related_model(join_relation)
-                query = query.join(model_relation)
-                # redefine order column name, because relationship can have a different name
-                # from the related table name.
-                tmp_order_column = tmp_order_column + model_relation.__tablename__ + '.'
-            order_column = tmp_order_column + order_column.split('.')[-1]
+                model_relation, relation_join = self.get_related_model_and_join(join_relation)
+                query = query.join(model_relation, relation_join, isouter=True)
         query_count = self.session.query(func.count('*')).select_from(self.obj)
 
         query_count = self._get_base_query(query=query_count,
@@ -421,6 +416,10 @@ class SQLAInterface(BaseInterface):
 
     def get_related_model(self, col_name):
         return self.list_properties[col_name].mapper.class_
+
+    def get_related_model_and_join(self, col_name):
+        relation = self.list_properties[col_name]
+        return relation.mapper.class_, relation.primaryjoin
 
     def query_model_relation(self, col_name):
         model = self.get_related_model(col_name)
