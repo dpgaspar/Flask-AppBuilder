5cf8fa6e1cb4cef2e8749ced8b4d843a414e5907
Daniel Gaspar <danielvazgaspar@gmail.com>
2019-05-03 15:22:04 +0100

    [examples] Fix, undo changes on crud rest api example

--- examples/crud_rest_api/app/api.py
+++ examples/crud_rest_api/app/api.py
@@ -20,36 +20,17 @@ fill_gender()
 
 class ContactModelApi(ModelRestApi):
     resource_name = "contact"
-    #class_permission_name = "api"
-    #previous_class_permission_name = "ContactModelApi"
     datamodel = SQLAInterface(Contact)
     allow_browser_login = True
-
-    #method_permission_name = {"get_list": "read",
-    #                          "get": "read",
-    #                          "info": "read"}
-    #previous_method_permission_name = {"get_list": "get",
-    #                                   "get": "get",
-    #                                   "info": "info"}
 
 
 appbuilder.add_api(ContactModelApi)
 
 
-
 class GroupModelApi(ModelRestApi):
     resource_name = "group"
-    #class_permission_name = "api"
-    #previous_class_permission_name = "GroupModelApi"
     datamodel = SQLAInterface(ContactGroup)
     allow_browser_login = True
 
-    #method_permission_name = {"get_list": "read",
-    #                          "get": "read",
-    #                          "info": "read"}
-    #previous_method_permission_name = {"get_list": "get",
-    #                                   "get": "get",
-    #                                   "info": "info"}
 
 appbuilder.add_api(GroupModelApi)
-
