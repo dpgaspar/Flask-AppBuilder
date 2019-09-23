c346ce44f05e3516a10320bb207760ff970f34dd
Daniel Gaspar <danielvazgaspar@gmail.com>
2019-09-23 08:42:24 +0100

    Fix, remove change on config example

--- examples/quickhowto/config.py
+++ examples/quickhowto/config.py
@@ -13,10 +13,9 @@ OPENID_PROVIDERS = [
     {"name": "MyOpenID", "url": "https://www.myopenid.com"},
 ]
 
-# SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "app.db")
+SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "app.db")
 # SQLALCHEMY_DATABASE_URI = 'mysql://username:password@mysqlserver.local/quickhowto'
 # SQLALCHEMY_DATABASE_URI = 'postgresql://scott:tiger@localhost:5432/myapp'
-SQLALCHEMY_DATABASE_URI = 'mssql+pymssql://sa:Password123@localhost:1433/myapp'
 # SQLALCHEMY_ECHO = True
 SQLALCHEMY_POOL_RECYCLE = 3
 
