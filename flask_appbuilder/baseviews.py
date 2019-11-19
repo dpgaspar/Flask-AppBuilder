42d4a8b3ec07fa5635b5de83620bbb6973d11c48
Cameron Nemo <cnorman@cenic.org>
2019-11-19 12:20:30 -0800

    [view] New, improve page/pane titles

--- flask_appbuilder/baseviews.py
+++ flask_appbuilder/baseviews.py
@@ -744,6 +744,8 @@ class BaseCRUDView(BaseModelView):
     """ Add widget override """
     show_widget = ShowWidget
     """ Show widget override """
+    title_column = None
+    """ Column to use for rendering the page and widget titles """
 
     actions = None
 
@@ -1007,6 +1009,7 @@ class BaseCRUDView(BaseModelView):
             formatters_columns=self.formatters_columns,
             actions=actions,
             fieldsets=show_fieldsets,
+            title_column=self.title_column,
             modelview_name=self.__class__.__name__,
         )
         return widgets
@@ -1030,6 +1033,7 @@ class BaseCRUDView(BaseModelView):
             include_cols=self.edit_columns,
             exclude_cols=exclude_cols,
             fieldsets=self.edit_fieldsets,
+            title_column=self.title_column,
         )
         return widgets
 
