913e9061155c7836d9c2dfa97a9ce296d89e2843
nutnamyang <nnamy@sertiscorp.com>
2020-05-12 10:45:19 +0700

    Hide Role Column in User Screen

--- flask_appbuilder/security/views.py
+++ flask_appbuilder/security/views.py
@@ -152,11 +152,11 @@ class UserModelView(ModelView):
                                'The user role on the application, this will associate with a list of permissions'),
                            'conf_password': lazy_gettext('Please rewrite the user\'s password to confirm')}
 
-    list_columns = ['first_name', 'last_name', 'username', 'email', 'active', 'roles', 'status']
+    list_columns = ['first_name', 'last_name', 'username', 'email', 'active', 'status']
 
     show_fieldsets = [
         (lazy_gettext('User info'),
-         {'fields': ['username', 'active', 'roles', 'login_count']}),
+         {'fields': ['username', 'active', 'login_count']}),
         (lazy_gettext('Personal Info'),
          {'fields': ['first_name', 'last_name', 'email'], 'expanded': True}),
         (lazy_gettext('Audit Info'),
@@ -166,7 +166,7 @@ class UserModelView(ModelView):
 
     user_show_fieldsets = [
         (lazy_gettext('User info'),
-         {'fields': ['username', 'active', 'roles', 'login_count']}),
+         {'fields': ['username', 'active', 'login_count']}),
         (lazy_gettext('Personal Info'),
          {'fields': ['first_name', 'last_name', 'email'], 'expanded': True}),
     ]
