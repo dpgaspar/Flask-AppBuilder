184f23a424e7e41e9258d087ee38f212aed3bdab
runoutnow <73939190+runoutnow@users.noreply.github.com>
2021-02-10 07:56:09 +0100

    [feature] updated for password reset_hash function

--- flask_appbuilder/security/views.py
+++ flask_appbuilder/security/views.py
@@ -841,9 +841,9 @@ class AuthOAuthView(AuthView):
                         redirect_uri=url_for(
                             ".oauth_authorized", provider=provider, _external=True
                         ),
-                         state=state.decode("ascii")
-                         if isinstance(state, bytes)
-                         else state,
+                        state=state.decode("ascii")
+                        if isinstance(state, bytes)
+                        else state,
                     )
             except Exception as e:
                 log.error("Error on OAuth authorize: {0}".format(e))
