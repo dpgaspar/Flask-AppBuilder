017c904af1781fb9f019e9896f7f0bc7d32072a7
runoutnow <73939190+runoutnow@users.noreply.github.com>
2021-02-13 14:14:32 +0100

    Update test_mvc.py

--- flask_appbuilder/tests/test_mvc.py
+++ flask_appbuilder/tests/test_mvc.py
@@ -1689,3 +1689,48 @@ class MVCTestCase(BaseMVCTestCase):
         self.assertEqual(state_transitions, target_state_transitions)
         role = self.appbuilder.sm.find_role("Test")
         self.assertEqual(len(role.permissions), 1)
+
+	
+class ResetHashMVCTestCase(BaseMVCTestCase):
+    """
+         Tests for Security reset password while a valid reset_hash with Email ack is required
+         (EMAIL_PROT = True)
+    """
+    def setUp(self):
+        super().setUp()
+        sess = PSSession()
+	
+        #turn password reset with hash function on
+        self.app.config["EMAIL_PROT"] = True
+        db = SQLA(self.app)
+        AppBuilder(self.app, db.session)
+
+        role_admin = self.appbuilder.sm.find_role("Admin")
+        self.appbuilder.sm.add_user(
+            "admin", "admin", "user", "admin@fab.org", role_admin, "general"
+        )
+
+	
+def tearDown(self):
+    self.appbuilder = None
+    self.app = None
+    self.db = None
+    log.debug("TEAR DOWN")
+
+
+
+def test_sec_reset_password_email_prot(self):
+    """
+    Test Security reset password while a valid reset_hash with Email ack is required
+    (EMAIL_PROT = True)
+    """
+    client = self.app.test_client()
+
+    # Try Reset My password while logged in, without a reset_hash with Email ack
+    rv = self.browser_login(client, USERNAME_ADMIN, PASSWORD_ADMIN)
+    rv = client.get("/users/action/resetmypassword/1", follow_redirects=True)
+    data = rv.data.decode("utf-8")
+    self.assertIn("Reset Password Form", data)
+    self.assertEqual(rv.status_code, 401)
+    self.browser_logout(client)
+	
