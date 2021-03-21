bfed65ff80ec56f58dcbbc1c33712ebf10b53490
Jonáš Jančařík <jonas.jancarik@gmail.com>
2021-03-21 21:59:46 +0100

    Update manager.py

--- flask_appbuilder/security/manager.py
+++ flask_appbuilder/security/manager.py
@@ -599,12 +599,12 @@ class BaseSecurityManager(AbstractSecurityManager):
             log.debug(str(id_token))
             me = self._azure_jwt_token_parse(id_token)
             log.debug("Parse JWT token : {0}".format(me))
-            upn_split = me["upn"].split("@") if "upn" in me else []
+            upn_split = me["upn"].split("@") if "upn" in me else None
             return {
                 "name": me["name"] if "name" in me else "N/A",
                 "email": me["upn"] if "upn" in me else "N/A",
-                "first_name": me["given_name"] if "given_name" in me else upn_split[0] if "upn" in me else "N/A",
-                "last_name": me["family_name"] if "family_name" in me else upn_split[1] if len(upn_split) > 1 else upn_split[0] if "upn" in me else "N/A",
+                "first_name": me["given_name"] if "given_name" in me else upn_split[0] if upn_split is not None else "N/A",
+                "last_name": me["family_name"] if "family_name" in me else upn_split[1] if len(upn_split) > 1 else upn_split[0] if upn_split is not None else "N/A",
                 "id": me["oid"],
                 "username": me["upn"].split("@")[0] if "upn" in me else me["oid"]
             }
