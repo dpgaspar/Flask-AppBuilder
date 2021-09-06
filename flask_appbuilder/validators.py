486859f0cc31f0c158736c901aca7e69666d8975
Daniel Gaspar <danielvazgaspar@gmail.com>
2021-09-06 13:49:35 +0100

    feat: password complexity option on DB Auth

--- flask_appbuilder/validators.py
+++ flask_appbuilder/validators.py
@@ -1,26 +1,45 @@
-from wtforms import ValidationError
+import re
+from typing import Optional
+
+from flask import current_app
+from flask_appbuilder.models.base import BaseInterface
+from flask_babel import gettext
+from wtforms import Field, Form, ValidationError
+
+password_complexity_regex = re.compile(
+    r"""(
+    ^(?=.*[A-Z].*[A-Z])                # at least two capital letters
+    (?=.*[!@#$&*])                     # at least one of these special characters
+    (?=.*[0-9].*[0-9])                 # at least two numeric digits
+    (?=.*[a-z].*[a-z].*[a-z])          # at least three lower case letters
+    .{10,}                              # at least 10 total digits
+    $
+    )""",
+    re.VERBOSE,
+)
 
 
-class Unique(object):
+class Unique:
     """
-        Checks field value unicity against specified table field.
-
-        :param datamodel:
-            The datamodel class, abstract layer for backend
-        :param col_name:
-            The unique column name.
-        :param message:
-            The error message.
+    WTForm field validator. Checks if field value is unique against
+    a specified table field.
     """
 
     field_flags = ("unique",)
 
-    def __init__(self, datamodel, col_name, message=None):
+    def __init__(
+        self, datamodel: BaseInterface, col_name: str, message: Optional[str] = None
+    ) -> None:
+        """
+        :param datamodel: The datamodel class, abstract layer for backend
+        :param col_name: The unique column name.
+        :param message: The error message.
+        """
         self.datamodel = datamodel
         self.col_name = col_name
         self.message = message
 
-    def __call__(self, form, field):
+    def __call__(self, form: Form, field: Field) -> None:
         filters = self.datamodel.get_filters().add_filter(
             self.col_name, self.datamodel.FilterEqual, field.data
         )
@@ -31,3 +50,35 @@ class Unique(object):
                 if self.message is None:
                     self.message = field.gettext(u"Already exists.")
                 raise ValidationError(self.message)
+
+
+class PasswordComplexityValidator:
+    """
+    WTForm field validator. Calls a custom password validator, useful for imposing
+    password complexity for database Auth users.
+    """
+
+    def __call__(self, form: Form, field: Field) -> None:
+        password_complexity_validator = current_app.config.get(
+            "FAB_PASSWORD_COMPLEXITY_VALIDATOR", None
+        )
+        if password_complexity_validator is not None:
+            password_complexity_validator(form, field)
+        if current_app.config.get("FAB_PASSWORD_COMPLEXITY_ENABLED", False):
+            default_password_complexity(form, field)
+
+
+def default_password_complexity(form: Form, field: Field) -> None:
+    """
+    FAB's default password complexity validator, set FAB_PASSWORD_COMPLEXITY_ENABLED
+    to True to enable it
+    """
+    match = re.search(password_complexity_regex, field.data)
+    if not match:
+        raise ValidationError(
+            gettext(
+                "Must have at least two capital letters,"
+                " one special character, two digits, three lower case letters and"
+                " a minimal length of 10."
+            )
+        )
