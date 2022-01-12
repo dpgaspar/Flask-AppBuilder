f77bc40830e0e0c4488c5f38ea0ecf76a05e8f28
Tzu-ping Chung <uranusjr@gmail.com>
2022-01-12 10:27:16 +0800

    Remove SQLAlchemy upper cap

--- setup.py
+++ setup.py
@@ -53,8 +53,6 @@ setup(
         "Flask-Babel>=1, <3",
         "Flask-Login>=0.3, <0.5",
         "Flask-OpenID>=1.2.5, <2",
-        # SQLAlchemy 1.4.0 breaks flask-sqlalchemy and sqlalchemy-utils
-        "SQLAlchemy<1.4.0",
         "Flask-SQLAlchemy>=2.4, <3",
         "Flask-WTF>=0.14.2, <0.15.0",
         "Flask-JWT-Extended>=3.18, <4",
