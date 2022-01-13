10502e5481ede85cd02e001300cda1e7f93be547
Daniel Gaspar <danielvazgaspar@gmail.com>
2022-01-13 12:14:43 +0000

    fix py versions on setup

--- setup.py
+++ setup.py
@@ -82,6 +82,8 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     python_requires="~=3.6",
