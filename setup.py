b27ef73b4f88b5b2fee677d3b6b348b6e84cab85
Daniel Vaz Gaspar <danielvazgaspar@gmail.com>
2022-01-19 09:45:35 +0000

    fix: Support SQLAlchemy 1.4.X (#1786)
    * fix: Support SQLAlchemy 1.4.X
    
    * fix new sqlalchemy already joined test for 1.4.X
    
    * fix setup.py
    
    * add sqlalchemy cap

--- setup.py
+++ setup.py
@@ -53,8 +53,6 @@ setup(
         "Flask-Babel>=1, <3",
         "Flask-Login>=0.3, <0.5",
         "Flask-OpenID>=1.2.5, <2",
-        # SQLAlchemy 1.4.0 breaks flask-sqlalchemy and sqlalchemy-utils
-        "SQLAlchemy<1.4.0",
         "Flask-SQLAlchemy>=2.4, <3",
         "Flask-WTF>=0.14.2, <0.15.0",
         "Flask-JWT-Extended>=3.18, <4",
@@ -65,6 +63,8 @@ setup(
         "python-dateutil>=2.3, <3",
         "prison>=0.2.1, <1.0.0",
         "PyJWT>=1.7.1, <2.0.0",
+        # Cautious cap
+        "SQLAlchemy<1.5",
         "sqlalchemy-utils>=0.32.21, <1",
         "WTForms<3.0.0",
     ],
