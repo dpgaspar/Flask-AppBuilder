5ae20c6e8e90c1408496e29e2ce5133755b9bbaf
Daniel Gaspar <danielvazgaspar@gmail.com>
2022-07-29 14:38:05 +0100

    debug test

--- flask_appbuilder/tests/test_mvc.py
+++ flask_appbuilder/tests/test_mvc.py
@@ -702,9 +702,10 @@ class MVCTestCase(BaseMVCTestCase):
             data = rv.data.decode("utf-8")
             self.assertNotIn('<label for="group"', data)
 
-            # Test direct edit view include related field
+            # Test direct edit view includes related field
             rv = client.get("/model2view/edit/1")
             data = rv.data.decode("utf-8")
+            raise Exception(data)
             self.assertIn('<label for="group"', data)
 
     def test_related_view_add_with_excluded_search(self):
