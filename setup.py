46675c4a6072bdf299c7f366bd86ad489d306853
Tomas Drtina <tm.drtina@gmail.com>
2022-08-11 22:21:56 +0200

    chore: Increase upper-bound on apispec

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     zip_safe=False,
     platforms="any",
     install_requires=[
-        "apispec[yaml]>=3.3, <4",
+        "apispec[yaml]>=3.3, <6",
         "colorama>=0.3.9, <1",
         "click>=8, <9",
         "email_validator>=1.0.5, <2",
