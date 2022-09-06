dpgaspar/Flask-AppBuilder

commit 01790618f5c263e34a2d0745402b61b9af297d49

fix(mvc): related model view setting default related field value (#1898)
* fix(mvc): related model view setting default related field value

* add tests

* debug test

* fix test

* revert examples change

* revert examples change

flask_appbuilder/baseviews.py
@@ -1196,7 +1196,7 @@ class BaseCRUDView(BaseModelView):
             returns add widget or None
         """
         is_valid_form = True
-        get_filter_args(self._filters)
+        get_filter_args(self._filters, disallow_if_not_in_search=False)
         exclude_cols = self._filters.get_relation_cols()
         form = self.add_form.refresh()
 
@@ -1232,7 +1232,7 @@ class BaseCRUDView(BaseModelView):
         pages = get_page_args()
         page_sizes = get_page_size_args()
         orders = get_order_args()
-        get_filter_args(self._filters)
+        get_filter_args(self._filters, disallow_if_not_in_search=False)
         exclude_cols = self._filters.get_relation_cols()
 
         item = self.datamodel.get(pk, self._base_filters)

flask_appbuilder/urltools.py
@@ -94,7 +94,16 @@ def get_order_args():
     return orders
 
 
-def get_filter_args(filters):
+def get_filter_args(filters, disallow_if_not_in_search=True):
+    """
+    Sets filters with the given current request args
+
+    Request arg filters are of the form "_flt_<DECIMAL>_<VIEW_NAME>_<COL_NAME>"
+
+    :param filters: Filter instance to apply the request filters on
+    :param disallow_if_not_in_search: If True, disallow filters that are not in the search
+    :return:
+    """
     filters.clear_filters()
     request_args = set(request.args)
     for arg in request_args:
@@ -103,10 +112,10 @@ def get_filter_args(filters):
             continue
         filter_index = int(re_match[0][0])
         filter_column = re_match[0][1]
-        if filter_column not in filters.get_search_filters().keys():
+        if (
+            filter_column not in filters.get_search_filters().keys()
+            and disallow_if_not_in_search
+        ):
             log.warning("Filter column not allowed")
             continue
-        if re_match:
-            filters.add_filter_index(
-                filter_column, filter_index, request.args.getlist(arg)
-            )
+        filters.add_filter_index(filter_column, filter_index, request.args.getlist(arg))

flask_appbuilder/views.py
@@ -326,7 +326,7 @@ class RestCRUDView(BaseCRUDView):
     @permission_name("add")
     def api_create(self):
         log.warning("This API is deprecated and will be removed on 2.3.X")
-        get_filter_args(self._filters)
+        get_filter_args(self._filters, disallow_if_not_in_search=False)
         exclude_cols = self._filters.get_relation_cols()
         form = self.add_form.refresh()
         self._fill_form_exclude_cols(exclude_cols, form)
@@ -354,7 +354,7 @@ class RestCRUDView(BaseCRUDView):
     @permission_name("edit")
     def api_update(self, pk):
         log.warning("This API is deprecated and will be removed on 2.3.X")
-        get_filter_args(self._filters)
+        get_filter_args(self._filters, disallow_if_not_in_search=False)
         exclude_cols = self._filters.get_relation_cols()
 
         item = self.datamodel.get(pk, self._base_filters)

examples/quickhowto/config.py
@@ -48,6 +48,7 @@ def custom_password_validator(password: str) -> None:
     if len(password) < 8:
         raise PasswordComplexityValidationError("Must have at least 8 characters")
 
+
 # FAB_PASSWORD_COMPLEXITY_VALIDATOR = custom_password_validator
 
 FAB_PASSWORD_COMPLEXITY_ENABLED = True
@@ -81,4 +82,3 @@ APP_THEME = ""  # default
 # APP_THEME = "sandstone.css"
 # APP_THEME = "solar.css"
 # APP_THEME = "superhero.css"
-

flask_appbuilder/tests/test_mvc.py
@@ -451,6 +451,7 @@ class MVCTestCase(BaseMVCTestCase):
             }
 
             order_columns = ["field_string", "group.field_string"]
+            search_exclude_columns = ["group"]
 
         class Model22View(ModelView):
             datamodel = SQLAInterface(Model2)
@@ -659,13 +660,13 @@ class MVCTestCase(BaseMVCTestCase):
 
     def test_fab_views(self):
         """
-            Test views creation and registration
+        Test views creation and registration
         """
         self.assertEqual(len(self.appbuilder.baseviews), 37)
 
     def test_back(self):
         """
-            Test Back functionality
+        Test Back functionality
         """
         with self.app.test_client() as c:
             self.browser_login(c, USERNAME_ADMIN, PASSWORD_ADMIN)
@@ -677,7 +678,7 @@ class MVCTestCase(BaseMVCTestCase):
 
     def test_model_creation(self):
         """
-            Test Model creation
+        Test Model creation
         """
         from sqlalchemy.engine.reflection import Inspector
 
@@ -688,6 +689,40 @@ class MVCTestCase(BaseMVCTestCase):
         self.assertIn("model2", inspector.get_table_names())
         self.assertIn("model3", inspector.get_table_names())
         self.assertIn("model_with_enums", inspector.get_table_names())
+
+    def test_related_view_edit_with_excluded_search(self):
+        """
+        Test related edit view with excluded search field
+        """
+        with self.app.test_client() as client:
+            self.browser_login(client, USERNAME_ADMIN, PASSWORD_ADMIN)
+
+            # Test excluded search field will not impact related view
+            rv = client.get("/model2view/edit/1?_flt_0_group=1")
+            data = rv.data.decode("utf-8")
+            self.assertNotIn('<label for="group"', data)
+
+            # Test direct edit view includes related field
+            rv = client.get("/model2view/edit/2")
+            data = rv.data.decode("utf-8")
+            self.assertIn('<label for="group"', data)
+
+    def test_related_view_add_with_excluded_search(self):
+        """
+        Test related add view with excluded search field
+        """
+        with self.app.test_client() as client:
+            self.browser_login(client, USERNAME_ADMIN, PASSWORD_ADMIN)
+
+            # Test excluded search field will not impact related view
+            rv = client.get("/model2view/add?_flt_0_group=1")
+            data = rv.data.decode("utf-8")
+            self.assertNotIn('<label for="group"', data)
+
+            # Test direct edit view include related field
+            rv = client.get("/model2view/add")
+            data = rv.data.decode("utf-8")
+            self.assertIn('<label for="group"', data)
 
     def test_index(self):
         """

flask_appbuilder/tests/test_urltools.py
@@ -45,6 +45,15 @@ class FlaskTestCase(FABTestCase):
             get_filter_args(filters)
             assert filters.values == []
 
+    def test_get_filter_args_disallow_off(self):
+        datamodel = SQLAInterface(Model1)
+        with self.appbuilder.get_app.test_request_context(
+            "/users/list?_flt_1_field_float=1.0"
+        ):
+            filters = datamodel.get_filters(["field_string", "field_integer"])
+            get_filter_args(filters, disallow_if_not_in_search=False)
+            assert filters.values == [["1.0"]]
+
     def test_get_filter_args_invalid_index(self):
         datamodel = SQLAInterface(Model1)
         with self.appbuilder.get_app.test_request_context(

commit a5f107e5d2675c0e8afd25685e0c4594cc181c31

fix(mvp): Address regression in #1898

flask_appbuilder/baseviews.py
@@ -1160,7 +1160,7 @@ class BaseCRUDView(BaseModelView):
             order_column, order_direction = "", ""
         page = get_page_args().get(self.__class__.__name__)
         page_size = get_page_size_args().get(self.__class__.__name__)
-        get_filter_args(self._filters)
+        get_filter_args(self._filters, disallow_if_not_in_search=False)
         widgets = self._get_list_widget(
             filters=self._filters,
             order_column=order_column,

flask_appbuilder/urltools.py
@@ -98,7 +98,7 @@ def get_filter_args(filters, disallow_if_not_in_search=True):
     """
     Sets filters with the given current request args
 
-    Request arg filters are of the form "_flt_<DECIMAL>_<VIEW_NAME>_<COL_NAME>"
+    Request arg filters are of the form "_flt_<INTEGER>_<VIEW_NAME>_<COL_NAME>"
 
     :param filters: Filter instance to apply the request filters on
     :param disallow_if_not_in_search: If True, disallow filters that are not in the search

flask_appbuilder/views.py
@@ -263,7 +263,7 @@ class RestCRUDView(BaseCRUDView):
             order_column, order_direction = "", ""
         page = get_page_args().get(self.__class__.__name__)
         page_size = get_page_size_args().get(self.__class__.__name__)
-        get_filter_args(self._filters)
+        get_filter_args(self._filters, disallow_if_not_in_search=False)
         joined_filters = self._filters.get_joined_filters(self._base_filters)
         count, lst = self.datamodel.query(
             joined_filters,