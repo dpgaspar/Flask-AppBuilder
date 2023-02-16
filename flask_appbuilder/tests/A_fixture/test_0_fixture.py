ea19c87225defb36ba6f6483957bed7c29c06c93
Bolke de Bruin <bolke@xs4all.nl>
2023-02-16 13:31:01 +0100

    Fix timeline when inserting

--- flask_appbuilder/tests/A_fixture/test_0_fixture.py
+++ flask_appbuilder/tests/A_fixture/test_0_fixture.py
@@ -26,15 +26,15 @@ class TestData(FABTestCase):
         self.appbuilder = AppBuilder(self.app, self.db.session)
 
     def test_data(self):
-        with Timeline(start=datetime(2020, 1, 1)).freeze():
+        with Timeline(start=datetime(2020, 1, 1), scale=0).freeze():
             insert_data(self.db.session, MODEL1_DATA_SIZE)
 
     def test_create_admin(self):
-        with Timeline(start=datetime(2020, 1, 1)).freeze():
+        with Timeline(start=datetime(2020, 1, 1), scale=0).freeze():
             self.create_admin_user(self.appbuilder, USERNAME_ADMIN, PASSWORD_ADMIN)
 
     def test_create_ro_user(self):
-        with Timeline(start=datetime(2020, 1, 1)).freeze():
+        with Timeline(start=datetime(2020, 1, 1), scale=0).freeze():
             self.create_user(
                 self.appbuilder,
                 USERNAME_READONLY,
