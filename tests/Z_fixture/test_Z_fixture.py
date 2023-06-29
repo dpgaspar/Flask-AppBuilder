b9116cc423df1c5551a5815b09df102cf0dcc80f
Daniel Gaspar <danielvazgaspar@gmail.com>
2023-06-30 00:08:13 +0100

    continue

--- tests/Z_fixture/test_Z_fixture.py
+++ tests/Z_fixture/test_Z_fixture.py
@@ -21,19 +21,19 @@ class TestData(FABTestCase):
         from flask_appbuilder import AppBuilder
 
         self.app = Flask(__name__)
-        self.app.config.from_object("flask_appbuilder.tests.config_api")
+        self.app.config.from_object("tests.config_api")
         self.db = SQLA(self.app)
         self.appbuilder = AppBuilder(self.app, self.db.session)
 
-    def test_data(self):
+    def _test_data(self):
         with Timeline(start=datetime(2020, 1, 1), scale=0).freeze():
             insert_data(self.db.session, MODEL1_DATA_SIZE)
 
-    def test_create_admin(self):
+    def _test_create_admin(self):
         with Timeline(start=datetime(2020, 1, 1), scale=0).freeze():
             self.create_admin_user(self.appbuilder, USERNAME_ADMIN, PASSWORD_ADMIN)
 
-    def test_create_ro_user(self):
+    def _test_create_ro_user(self):
         with Timeline(start=datetime(2020, 1, 1), scale=0).freeze():
             self.create_user(
                 self.appbuilder,
