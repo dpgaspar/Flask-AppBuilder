0ff35530b0b16273bac0215043559858fbec22c2
goerch <jhr.walter@t-online.de>
2023-11-10 13:28:00 +0100

    Activating form tests for Enum and Float

--- tests/test_forms.py
+++ tests/test_forms.py
@@ -40,8 +40,8 @@ class FieldsModel(Model):
     field_boolean = Column(Boolean())
     field_date = Column(Date())
     field_datetime = Column(DateTime())
-    # field_enum = Column(Enum())
-    # field_float = Column(Float())
+    field_enum = Column(Enum())
+    field_float = Column(Float())
     field_integer = Column(Integer())
     field_numeric_scale0 = Column(Numeric())
     # field_numeric_scale2 = Column(Numeric(scale=2))
@@ -66,8 +66,8 @@ class FlaskTestCase(unittest.TestCase):
             "field_boolean",
             "field_date",
             "field_datetime",
-            # "field_enum",
-            # "field_float",
+            "field_enum",
+            "field_float",
             "field_integer",
             "field_numeric_scale0",
             # "field_numeric_scale2",
@@ -79,8 +79,8 @@ class FlaskTestCase(unittest.TestCase):
         self.assertTrue(form.field_boolean.field_class is BooleanField)
         self.assertTrue(form.field_date.field_class is DateField)
         self.assertTrue(form.field_datetime.field_class is DateTimeField)
-        # self.assertTrue(form.field_enum.field_class is EnumField)
-        # self.assertTrue(form.field_float.field_class is FloatField)
+        self.assertTrue(form.field_enum.field_class is EnumField)
+        self.assertTrue(form.field_float.field_class is FloatField)
         self.assertTrue(form.field_integer.field_class is IntegerField)
         self.assertTrue(form.field_numeric_scale0.field_class is DecimalField) # and not form.field_numeric_scale0.kwargs["places"])
         # self.assertTrue(form.field_numeric_scale2.field_class is DecimalField and form.field_numeric_scale2.kwargs["places"] == 2)
@@ -95,8 +95,8 @@ class FlaskTestCase(unittest.TestCase):
             "field_boolean",
             "field_date",
             "field_datetime",
-            # "field_enum",
-            # "field_float",
+            "field_enum",
+            "field_float",
             "field_integer",
             "field_numeric_scale0",
             # "field_numeric_scale2",
@@ -109,8 +109,8 @@ class FlaskTestCase(unittest.TestCase):
             self.assertTrue(isinstance(form.field_boolean, BooleanField))
             self.assertTrue(isinstance(form.field_date, DateField))
             self.assertTrue(isinstance(form.field_datetime, DateTimeField))
-            # self.assertTrue(isinstance(form.field_enum, EnumField))
-            # self.assertTrue(isinstance(form.field_float, FloatField))
+            self.assertTrue(isinstance(form.field_enum, EnumField))
+            self.assertTrue(isinstance(form.field_float, FloatField))
             self.assertTrue(isinstance(form.field_integer, IntegerField))
             self.assertTrue(isinstance(form.field_numeric_scale0, DecimalField)) # and not form.field_numeric_scale0.places)
             # self.assertTrue(isinstance(form.field_numeric_scale2, DecimalField) and form.field_numeric_scale2.places == 2)
