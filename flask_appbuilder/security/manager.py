b232e10bbdf9e0efa2aa594053f96e6232c1d192
Daniel Vaz Gaspar <danielvazgaspar@gmail.com>
2025-02-19 15:12:41 +0000

    fix: werkzeug version check (#2310)
    * fix: werkzeug version check
    
    * fix

--- flask_appbuilder/security/manager.py
+++ flask_appbuilder/security/manager.py
@@ -1,4 +1,5 @@
 import datetime
+import importlib
 import logging
 import re
 from typing import Any, Callable, Dict, List, Optional, Set, Tuple, Union
@@ -12,6 +13,7 @@ from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from flask_login import current_user, LoginManager
 import jwt
+from packaging.version import Version
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from .api import SecurityApi
@@ -230,10 +232,8 @@ class BaseSecurityManager(AbstractSecurityManager):
         app.config.setdefault("AUTH_ROLES_SYNC_AT_LOGIN", False)
         app.config.setdefault("AUTH_API_LOGIN_ALLOW_MULTIPLE_PROVIDERS", False)
 
-        from packaging.version import Version
-        from werkzeug import __version__ as werkzeug_version
-
-        parsed_werkzeug_version = Version(werkzeug_version)
+        # Werkzeug prior to 3.0.0 does not support scrypt
+        parsed_werkzeug_version = Version(importlib.metadata.version("werkzeug"))
         if parsed_werkzeug_version < Version("3.0.0"):
             app.config.setdefault(
                 "AUTH_DB_FAKE_PASSWORD_HASH_CHECK",
